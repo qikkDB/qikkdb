d1e1d535da6f6fb8f8a6c160e4cb17cba4b25cb9
Andrej Fusek <andrej.fusek@gmail.com>
2019-01-17 15:07:32 +0100

    Add spdlog configuration [14713]

--- dropdbase/main.cpp
+++ dropdbase/main.cpp
@@ -1,4 +1,21 @@
+#include <spdlog_setup/conf.h>
+
 int main(int argc, char** argv)
 {
+	try {
+		spdlog_setup::from_file("../configuration/log_config.toml"); // configure logger
+		auto log_ = spdlog::get("root"); // get logger
+		log_->info("Application started.");
+
+		// TODO main loop
+
+		log_->info("Application exited.");
+	}
+	catch (const spdlog_setup::setup_error &exception) { // log config file not found
+		printf("Cannot setup logger: %s", exception.what());
+	}
+	catch (const std::exception &exception) {
+		printf("Exception: %s", exception.what());
+	}
 	return 0;
 }
