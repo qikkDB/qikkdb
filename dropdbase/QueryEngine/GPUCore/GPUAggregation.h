8f372994ad3dbe8fd0b3b48567adefc582eb025f
Andrej Fusek <andrej.fusek@gmail.com>
2019-01-18 11:18:33 +0100

    Change project type to CUDA
    Via Kosice, but it is working

--- dropdbase/QueryEngine/GPUCore/GPUAggregation.h
+++ dropdbase/QueryEngine/GPUCore/GPUAggregation.h
@@ -1,6 +1,9 @@
 #ifndef GPU_AGGREGATION_H
 #define GPU_AGGREGATION_H
 
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
 #include "../IAggregation.h"
 
 class GPUAggregation : public IAggregation {
