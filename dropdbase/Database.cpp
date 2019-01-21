d58d49eab2732bf2620aa7af6478bd8ba1f51f07
Dávid Kubík <kubik.david5@gmail.com>
2019-01-21 16:54:57 +0100

    WIP Database.cpp

--- dropdbase/Database.cpp
+++ dropdbase/Database.cpp
@@ -5,6 +5,7 @@
 
 #include "Database.h"
 #include "Configuration.h"
+#include "ColumnBase.h"
 
 std::unordered_map<std::string, std::shared_ptr<Database>> Database::loadedDatabases_;
 
@@ -13,7 +14,7 @@ std::unordered_map<std::string, std::shared_ptr<Database>> Database::loadedDatab
 /// </summary>
 /// <param name="databaseName">Database name.</param>
 /// <param name="blockSize">Block size of all blocks in this database</param>
-Database::Database(std::string databaseName, int blockSize) : log_(spdlog::get("root"))
+Database::Database(const char* databaseName, int blockSize) : log_(spdlog::get("root"))
 {
 	name_ = databaseName;
 	blockSize_ = blockSize;
@@ -33,169 +34,215 @@ void Database::Persist(const char * path)
 
 	if (std::filesystem::create_directory(path))
 	{
+		int blockSize = GetBlockSize();
+		int tableSize = tables.size();
+
 		//write file .db
 		log_->debug("Saving .db file with name: '{}'", pathStr + name + ".db");
 		std::ofstream dbFile(pathStr + name + ".db", std::ios::binary);
-		dbFile.write(name.c_str(), name.length); //argumetns: buffer, size
-		dbFile.write((char*) GetBlockSize(), sizeof(unsigned int));
-		dbFile.write((char*) tables.size(), sizeof(unsigned int));
-		for (auto table : tables)
+
+		int dbNameLength = name.length();
+
+		dbFile.write(reinterpret_cast<char*>(&dbNameLength), sizeof(int)); //write db name length
+		dbFile.write(name.c_str(), name.length()); //write db name
+		dbFile.write(reinterpret_cast<char*>(&blockSize), sizeof(int)); //write block size
+		dbFile.write(reinterpret_cast<char*>(&tableSize), sizeof(int)); //write number of tables
+		for (auto& table : tables)
 		{
 			auto columns = table.second.GetColumns();
-			dbFile.write(table.first.c_str(), sizeof(table.first.c_str())); //write table name
-			dbFile.write(columns.size(), sizeof(unsigned int)); //number of columns of the table
-			for (auto column : columns)
+			int tableNameLength = table.first.length();
+
+			dbFile.write(reinterpret_cast<char*>(&tableNameLength), sizeof(int)); //write table name length
+			dbFile.write(table.first.c_str(), tableNameLength); //write table name
+			dbFile.write(reinterpret_cast<char*>(columns.size()), sizeof(int)); //write number of columns of the table
+			for (const auto& column : columns)
 			{
-				dbFile.write(column.first.c_str(), sizeof(column.first.c_str())); //name of the column
+				int columnNameLength = column.first.length();
+
+				dbFile.write(reinterpret_cast<char*>(&columnNameLength), sizeof(int)); //write column name length
+				dbFile.write(column.first.c_str(), columnNameLength); //write column name
 			}
 		}
 		dbFile.close();
 
 		//write files .col
-		for (auto table : tables)
+		for (auto& table : tables)
 		{
 			auto columns = table.second.GetColumns();
 
-			for (auto column : columns)
+			for (const auto& column : columns)
 			{
-				log_->debug("Saving .col file with name: '{}'", pathStr + name + "_" + table.first + "_" + column.GetName() + ".col");
+				log_->debug("Saving .col file with name: '{}'", pathStr + name + "_" + table.first + "_" + column.second->GetName() + ".col");
 
-				std::ofstream colFile(pathStr + name + "_" + table.first + "_" + column.GetName() + ".col", std::ios::binary);
+				std::ofstream colFile(pathStr + name + "_" + table.first + "_" + column.second->GetName() + ".col", std::ios::binary);
 
-				auto type = std::string(column.GetColumnType());
+				int type = column.second->GetColumnType();
 
-				colFile.write(type.c_str(), sizeof(type.c_str())); //type of column
+				colFile.write(reinterpret_cast<char*>(&type), sizeof(int)); //write type of column
 				
-				unsigned int index = 0;
-				for (auto block : column.GetBlocks())
+				switch (type)
 				{
-					//save block of type - String
-					if (type == "String")
-					{
-						log_->debug("Saving block of Point data with index = {}.", index);
+				case COLUMN_INT:
+					unsigned int index = 0;
 
-						auto dataStr = block.GetData();
-
-						if (dataStr != nullptr)
-						{
-							colFile.write((char*)index, sizeof(unsigned int)); //write index
-							colFile.write((char*)dataStr.length, sizeof(unsigned int)); //write block length
-							for (auto entry : dataStr) //write data of block
-							{
-								colFile.write((char*)entry.c_str, entry.length);
-							}
-							index += 1;
-						}
-					}
-					//save block of type - ComplexPolygon:
-					else if (type == "ComplexPolygon")
-					{
-						log_->debug("Saving block of ComplexPolygon data with index = {}.", index);
-
-						auto dataStr = block.GetData();
-
-						if (dataStr != nullptr)
-						{
-							colFile.write((char*)index, sizeof(unsigned int)); //write index
-							colFile.write((char*)dataStr.length, sizeof(unsigned int)); //write block length
-							for (auto entry : dataStr) //write data of block
-							{
-								colFile.write((char*)entry.c_str, entry.length);
-							}
-							index += 1;
-						}
-					}
-					//save block of type - Point:
-					else if (type == "Point")
-					{
-						log_->debug("Saving block of Point data with index = {}.", index);
-
-						auto dataStr = block.GetData();
-
-						if (dataStr != nullptr)
-						{
-							colFile.write((char*)index, sizeof(unsigned int)); //write index
-							colFile.write((char*)dataStr.length, sizeof(unsigned int)); //write block length
-							for (auto entry : dataStr) //write data of block
-							{
-								colFile.write((char*)entry.c_str, entry.length);
-							}
-							index += 1;
-						}
-					}
-					//save block of type - Integer:
-					else if (type == "Int")
+					const ColumnBase<int>* colInt = dynamic_cast<const ColumnBase<int>*>(column.second.get());
+					
+					for (const auto& block : colInt->GetBlocksList())
 					{
 						log_->debug("Saving block of Integer data with index = {}.", index);
 
-						auto data = block.GetData();
+						auto& data = block->GetData();
+						int dataLength = data.size();
 
-						if (data != nullptr)
+						if (data.size() > 0)
 						{
-							colFile.write((char*)index, sizeof(unsigned int)); //write index
-							colFile.write((char*)data.length, sizeof(unsigned int)); //write block length
-							for (auto entry : data) //write data of block
+							colFile.write(reinterpret_cast<char*>(&index), sizeof(unsigned int)); //write index
+							colFile.write(reinterpret_cast<char*>(dataLength), sizeof(unsigned int)); //write block length
+							for (auto entry : data)
 							{
-								colFile.write((char*)entry, sizeof(int));
+								colFile.write(reinterpret_cast<char*>(&entry), sizeof(int)); //write data of block
 							}
 							index += 1;
 						}
 					}
-					//save block of type - Long:
-					else if (type == "Long")
-					{
-						log_->debug("Saving block of Long data with index = {}.", index);
 
-						auto data = block.GetData();
+					break;
 
-						if (data != nullptr)
-						{
-							colFile.write((char*)index, sizeof(unsigned int)); //write index
-							colFile.write((char*)data.length, sizeof(unsigned int)); //write block length
-							for (auto entry : data) //write data of block
-							{
-								colFile.write((char*)entry, sizeof(long));
-							}
-							index += 1;
-						}
-					}
-					//save block of type - Double:
-					else if (type == "Double")
-					{
-						log_->debug("Saving block of Double data with index = {}.", index);
-
-						auto data = block.GetData();
-
-						if (data != nullptr)
-						{
-							colFile.write((char*)index, sizeof(unsigned int)); //write index
-							colFile.write((char*)data.length, sizeof(unsigned int)); //write block length
-							for (auto entry : data) //write data of block
-							{
-								colFile.write((char*)entry, sizeof(double));
-							}
-							index += 1;
-						}
-					}
-					//save block of type - Float:
-					else if (type == "Float")
-					{
-						log_->debug("Saving block of Float data with index = {}.", index);
-
-						auto data = block.GetData();
-
-						if (data != nullptr)
-						{
-							colFile.write((char*)index, sizeof(unsigned int)); //write index
-							colFile.write((char*)data.length, sizeof(unsigned int)); //write block length
-							for (auto entry : data) //write data of block
-							{
-								colFile.write((char*)entry, sizeof(float));
-							}
-							index += 1;
-						}
-					}
+					//TODO tu pojdu vsetky cases
 				}
+
+				//for (auto block : column.GetBlocks())
+				//{
+				//	//save block of type - String
+				//	if (type == "String")
+				//	{
+				//		log_->debug("Saving block of Point data with index = {}.", index);
+
+				//		auto dataStr = block.GetData();
+
+				//		if (dataStr != nullptr)
+				//		{
+				//			int strLength = dataStr.length();
+
+				//			colFile.write(reinterpret_cast<char*>(&index), sizeof(unsigned int)); //write index
+				//			dbFile.write(reinterpret_cast<char*>(&strLength), sizeof(int)); //write string length
+				//			colFile.write(static_cast<char*>(dataStr.length), strLength); //write block length
+				//			for (auto entry : dataStr) //write data of block
+				//			{
+				//				colFile.write(static_cast<char*>(entry.c_str()), entry.length);
+				//			}
+				//			index += 1;
+				//		}
+				//	}
+				//	//save block of type - ComplexPolygon:
+				//	else if (type == "ComplexPolygon")
+				//	{
+				//		log_->debug("Saving block of ComplexPolygon data with index = {}.", index);
+
+				//		auto dataStr = block.GetData();
+
+				//		if (dataStr != nullptr)
+				//		{
+				//			colFile.write(reinterpret_cast<char*>(&index), sizeof(unsigned int)); //write index
+				//			colFile.write(static_cast<char*>(dataStr.length), sizeof(unsigned int)); //write block length
+				//			for (auto entry : dataStr) //write data of block
+				//			{
+				//				colFile.write(static_cast<char*>(entry.c_str()), entry.length);
+				//			}
+				//			index += 1;
+				//		}
+				//	}
+				//	//save block of type - Point:
+				//	else if (type == "Point")
+				//	{
+				//		log_->debug("Saving block of Point data with index = {}.", index);
+
+				//		auto dataStr = block.GetData();
+
+				//		if (dataStr != nullptr)
+				//		{
+				//			colFile.write(reinterpret_cast<char*>(&index), sizeof(unsigned int)); //write index
+				//			colFile.write(static_cast<char*>(dataStr.length), sizeof(unsigned int)); //write block length
+				//			for (auto entry : dataStr) //write data of block
+				//			{
+				//				colFile.write(static_cast<char*>(entry.c_str()), entry.length);
+				//			}
+				//			index += 1;
+				//		}
+				//	}
+				//	//save block of type - Integer:
+				//	else if (type == "Int")
+				//	{
+				//		log_->debug("Saving block of Integer data with index = {}.", index);
+
+				//		auto data = block.GetData();
+
+				//		if (data != nullptr)
+				//		{
+				//			colFile.write(reinterpret_cast<char*>(&index), sizeof(unsigned int)); //write index
+				//			colFile.write(static_cast<char*>(data.length), sizeof(unsigned int)); //write block length
+				//			for (auto entry : data) //write data of block
+				//			{
+				//				colFile.write(static_cast<char*>(entry), sizeof(int));
+				//			}
+				//			index += 1;
+				//		}
+				//	}
+				//	//save block of type - Long:
+				//	else if (type == "Long")
+				//	{
+				//		log_->debug("Saving block of Long data with index = {}.", index);
+
+				//		auto data = block.GetData();
+
+				//		if (data != nullptr)
+				//		{
+				//			colFile.write(reinterpret_cast<char*>(&index), sizeof(unsigned int)); //write index
+				//			colFile.write(static_cast<char*>(data.length), sizeof(unsigned int)); //write block length
+				//			for (auto entry : data) //write data of block
+				//			{
+				//				colFile.write(static_cast<char*>(entry), sizeof(long));
+				//			}
+				//			index += 1;
+				//		}
+				//	}
+				//	//save block of type - Double:
+				//	else if (type == "Double")
+				//	{
+				//		log_->debug("Saving block of Double data with index = {}.", index);
+
+				//		auto data = block.GetData();
+
+				//		if (data != nullptr)
+				//		{
+				//			colFile.write(reinterpret_cast<char*>(&index), sizeof(unsigned int)); //write index
+				//			colFile.write(static_cast<char*>(data.length), sizeof(unsigned int)); //write block length
+				//			for (auto entry : data) //write data of block
+				//			{
+				//				colFile.write(static_cast<char*>(entry), sizeof(double));
+				//			}
+				//			index += 1;
+				//		}
+				//	}
+				//	//save block of type - Float:
+				//	else if (type == "Float")
+				//	{
+				//		log_->debug("Saving block of Float data with index = {}.", index);
+
+				//		auto data = block.GetData();
+
+				//		if (data != nullptr)
+				//		{
+				//			colFile.write(reinterpret_cast<char*>(&index), sizeof(unsigned int)); //write index
+				//			colFile.write(static_cast<char*>(data.length), sizeof(unsigned int)); //write block length
+				//			for (auto entry : data) //write data of block
+				//			{
+				//				colFile.write(static_cast<char*>(entry), sizeof(float));
+				//			}
+				//			index += 1;
+				//		}
+				//	}
+				//}
 			}
 		}
 
@@ -224,21 +271,49 @@ void Database::SaveAllToDisk(const char * path)
 /// </summary>
 void Database::LoadDatabasesFromDisk()
 {
-	struct stat info;
 	auto path = Configuration::DatabaseDir();
 
-	if (stat(path, &info) != 0)
-	{
-		log_->error("Cannot access path: {}.", path);
-	}
-	else if (info.st_mode & S_IFDIR)
-	{
-		//TODO dorobit telo procedury !!!!!!!!!!!
+	if (std::filesystem::exists(path)) {
+		for (auto& p : std::filesystem::directory_iterator(path))
+		{
+			auto extension = p.path().extension();
+			if (extension == ".db")
+			{
+				auto database = Database::LoadDatabase(p.path().filename().stem().generic_string().c_str(), path);
+
+				if (database != nullptr)
+				{
+					loadedDatabases_.insert( {database->name_, database} );
+				}
+			}
+		}
 	}
 	else
 	{
 		log_->error("Directory {} does not exists.", path);
 	}
+}
+
+std::shared_ptr<Database> Database::LoadDatabase(const char * fileDbName, const char * path)
+{
+	log_->info("Loading database from directory: {} with file name: {}.", path, fileDbName);
+
+	//read file .db
+	std::ifstream dbFile(path + std::string(fileDbName) + ".db", std::ios::binary);
+	
+	std::unique_ptr<char[]> dbName = std::make_unique<char[]>(length); //dynamic allocation
+	dbFile.read(dbName, 5);
+	auto blockSize = dbFile.get();
+	auto tablesCount = dbFile.get();
+
+	Database database(dbName, blockSize);
+
+	for (auto i = 0; i < tablesCount; i++)
+	{
+		//TODO dorobit !!!!!!!!!!!!!!
+	}
+
+	return std::shared_ptr<Database>();
 }
 
 void Database::AddToInMemoryDatabaseList(std::shared_ptr<Database> database)
