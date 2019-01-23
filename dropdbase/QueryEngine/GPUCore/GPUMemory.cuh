13acac85dce17829bf9402e229af10df98ccb5b9
Nikolas Hamran <nikolas.hamran96@gmail.com>
2019-01-23 10:14:31 +0100

    Finished memory operations with templates

--- dropdbase/QueryEngine/GPUCore/GPUMemory.cuh
+++ dropdbase/QueryEngine/GPUCore/GPUMemory.cuh
@@ -7,25 +7,36 @@
 #include "device_launch_parameters.h"
 
 #include "../Context.h"
+#include "../CudaMemAllocator.h"
 
+// Memory methods
 class GPUMemory {
 public:
-	// Memory methods
-
 	// Memory allocation
 	template<typename T>
 	void alloc(T **p_Block, int32_t dataElementCount) const
 	{
-		cudaError_t cudaStatus = cudaMalloc(p_Block, dataElementCount * sizeof(T));
-		Context::getInstance().getLastError().setCudaError(cudaStatus);
+		*p_Block = reinterpret_cast<T*>(CudaMemAllocator::GetInstance().allocate(dataElementCount * sizeof(T)));
+		Context::getInstance().getLastError().setCudaError(cudaSuccess);
 	}
 
 	// malloc + memset
 	template<typename T>
-	void allocAndSet(T **p_Block, int32_t size, int32_t dataElementCount, T value) const
+	void allocAndSet(T **p_Block, T value, int32_t dataElementCount) const
 	{
-		cudaError_t cudaStatus = cudaCalloc(p_Block, dataElementCount * sizeof(T));
-		cudaStatus = cudaMemset(ptr, value, dataElementCount * sizeof(T));
+		*p_Block = CudaMemAllocator::GetInstance().allocate(dataElementCount * sizeof(T));
+
+		fill(*p_Block, value, dataElementCount);
+
+		Context::getInstance().getLastError().setCudaError(cudaStatus);
+	}
+
+	// Fill an array with a desired value
+	template<typename T>
+	void fill(T *p_Block, T value, int32_t dataElementCount) const
+	{
+		//cudaError_t cudaStatus = cudaMemsetAsync(p_Block, value, dataElementCount * sizeof(T));	// Async version, uncomment if needed
+		cudaError_t cudaStatus = cudaMemset(p_Block, value, dataElementCount * sizeof(T));
 		Context::getInstance().getLastError().setCudaError(cudaStatus);
 	}
 
@@ -33,7 +44,7 @@ public:
 	template<typename T>
 	void copyHostToDevice(T *p_BlockDevice, void *p_BlockHost, int32_t dataElementCount) const
 	{
-		cudaError_t cudaStatus = cudaMemcpy(p_BlockDevice, p_BlockHost, dataElementCount * sizeof(T), 
+		cudaError_t cudaStatus = cudaMemcpy(p_BlockDevice, p_BlockHost, dataElementCount * sizeof(T),
 			cudaMemcpyHostToDevice);
 		Context::getInstance().getLastError().setCudaError(cudaStatus);
 	}
@@ -42,7 +53,7 @@ public:
 	template<typename T>
 	void copyDeviceToHost(T *p_BlockHost, void *p_BlockDevice, int32_t dataElementCount) const
 	{
-		cudaError_t cudaStatus = cudaMemcpy(p_BlockHost, p_BlockDevice, dataElementCount * sizeof(T), 
+		cudaError_t cudaStatus = cudaMemcpy(p_BlockHost, p_BlockDevice, dataElementCount * sizeof(T),
 			cudaMemcpyDeviceToHost);
 		Context::getInstance().getLastError().setCudaError(cudaStatus);
 	}
@@ -51,15 +62,39 @@ public:
 	template<typename T>
 	void free(T *p_block) const
 	{
-		cudaError_t cudaStatus = cudaFree(p_block);
+		CudaMemAllocator::GetInstance().deallocate(reinterpret_cast<int8_t*>(p_block), 0);
+		Context::getInstance().getLastError().setCudaError(cudaSuccess);
+	}
+
+	template<typename T>
+	void hostRegister(T **devicePtr, T *hostPtr, int32_t dataElementCount)
+	{
+		cudaError_t cudaStatus = cudaHostRegister(hostPtr, dataElementCount * sizeof(T), cudaHostRegisterMapped);
+		cudaStatus = cudaHostGetDevicePointer(devicePtr, hostPtr, 0);
+
 		Context::getInstance().getLastError().setCudaError(cudaStatus);
 	}
 
 	template<typename T>
-	void hostRegister(T **devicePtr, void *hostPtr, int32_t dataElementCount) const;
+	void hostUnregister(T *hostPtr)
+	{
+		cudaError_t cudaStatus = cudaHostUnregister(hostPtr);
+		Context::getInstance().getLastError().setCudaError(cudaStatus);
+	}
 
+	// Pin host memory
 	template<typename T>
-	void hostUnregister(T *hostPtr) const;
+	void hostPin(T* hostPtr, int32_t dataElementCount)
+	{
+		cudaError_t cudaStatus = cudaHostRegister(hostPtr, dataElementCount * sizeof(T), cudaHostRegisterDefault);
+		Context::getInstance().getLastError().setCudaError(cudaStatus);
+	}
+
+	// Wipe all allocated memory O(1)
+	void clear()
+	{
+		CudaMemAllocator::GetInstance().Clear();
+	}
 
 };
 
--- dropdbase/main.cpp
+++ dropdbase/main.cpp
@@ -1,5 +1,8 @@
+#include "QueryEngine/Context.h"
+#include "QueryEngine/EngineCore.h"
+#include "QueryEngine/GPUCore/GPUMemory.cuh"
+
 int main(int argc, char** argv)
 {
-	
 	return 0;
 }
