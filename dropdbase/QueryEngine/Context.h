f14c04bf89489910515275d88bff2541e1d76945
Nikolas Hamran <nikolas.hamran96@gmail.com>
2019-01-23 12:52:47 +0100

    Added reconstruct, not complete, detached engine core from context

--- dropdbase/QueryEngine/Context.h
+++ dropdbase/QueryEngine/Context.h
@@ -11,22 +11,17 @@
 
 #include "QueryEngineError.h"
 
-class EngineCore;
-
 class Context {
 private:
 	// Limit on the grid size
-	static constexpr int32_t DEFAULT_GRID_DIMENSION_LIMIT = 65535;
+	static const int32_t DEFAULT_GRID_DIMENSION_LIMIT = 65535;
 
 	// Default grid dimensions for kernel launching
-	static constexpr int32_t DEFAULT_GRID_DIMENSION = 65535;
-	static constexpr int32_t DEFAULT_BLOCK_DIMENSION = 1024;
+	static const int32_t DEFAULT_GRID_DIMENSION = 65535;
+	static const int32_t DEFAULT_BLOCK_DIMENSION = 1024;
 
 	// Number of opitimal threads per block queried for a specific GPU - currently bound to the context
 	int32_t queried_block_dimension_;
-
-	// Engine core for operations over the context - GPU only - pointer because of cyclic dependency
-	std::unique_ptr<EngineCore> engineCore_;
 
 	// Registry for holding the last error
 	QueryEngineError lastError_;
@@ -37,35 +32,40 @@ private:
 	std::vector<cudaDeviceProp> devicesMetaInfo_;
 
 	// Meyer's singleton
-	Context(std::unique_ptr<EngineCore> engineCore) :
-		engineCore_(std::move(engineCore)),
-		queried_block_dimension_(DEFAULT_BLOCK_DIMENSION)
-	{
-
-	}
+	Context() : queried_block_dimension_(DEFAULT_BLOCK_DIMENSION) {};
 	~Context() = default;
 	Context(const Context&) = delete;
 	Context& operator=(const Context&) = delete;
 
 public:
-
 	// Get class instance, if class was not initialized prior a GPU instance is returned
-	static Context& getInstance();
-
-	// Get the engine core interface for kernel operations
-	const EngineCore& getEngineCore() const { return *engineCore_; }
+	static Context& Context::getInstance()
+	{
+		// Static instance - constructor called only once
+		static Context instance;
+		return instance;
+	}
 
 	// Get the last cuda error
 	QueryEngineError& getLastError() { return lastError_; }
 
 	// Operations on the grid dimensions
-	int32_t calcGridDim(int32_t threadCount);
+	int32_t Context::calcGridDim(int32_t threadCount)
+	{
+		int blockCount = (threadCount + queried_block_dimension_ - 1) / queried_block_dimension_;
+		if (blockCount >= (DEFAULT_GRID_DIMENSION_LIMIT + 1))
+		{
+			blockCount = DEFAULT_GRID_DIMENSION_LIMIT;
+		}
+		return blockCount;
+	}
 
 	int32_t getBlockDim() { return DEFAULT_BLOCK_DIMENSION; }
 
 	// Querying info about devices and rebinding devices to the context
 	const std::vector<cudaDeviceProp>& getDevicesMetaInfoList() const { return devicesMetaInfo_; }
 
+	// Bind device to context if neccessary
 	void bindDeviceToContext(int32_t deviceID) { }
 
 };
