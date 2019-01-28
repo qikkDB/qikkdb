677fdfb0c4e615563126961bb26df3d32f18f245
Andrej Fusek <andrej.fusek@gmail.com>
2019-01-28 16:17:34 +0100

    GPUArithemticConst: Add reversed (const-col) operations

--- dropdbase/QueryEngine/GPUCore/GPUArithmeticConst.cuh
+++ dropdbase/QueryEngine/GPUCore/GPUArithmeticConst.cuh
@@ -77,6 +77,37 @@ __global__ void kernel_minus_const(T *output, U *ACol, V BConst, int32_t dataEle
 }
 
 /// <summary>
+/// Kernel MINUS (constant - column)
+/// </summary>
+/// <param name="output">output result data block</param>
+/// <param name="AConst">left input operand</param>
+/// <param name="BCol">block of the right input operands</param>
+/// <param name="dataElementCount">count of elements in the input block</param>
+/// <param name="errorFlag">flag for error checking</param>
+template<typename T, typename U, typename V>
+__global__ void kernel_minus_const_col(T *output, U AConst, V *BCol, int32_t dataElementCount, int32_t* errorFlag, T min, T max)
+{
+	int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+	int32_t stride = blockDim.x * gridDim.x;
+
+	for (int32_t i = idx; i < dataElementCount; i += stride)
+	{
+		// if none of the input operands are float
+		if (!std::is_floating_point<U>::value && !std::is_floating_point<V>::value)
+		{
+			// Check for overflow
+			if (((BCol[i] > V{ 0 }) && (AConst < (min + BCol[i]))) ||
+				((BCol[i] < V{ 0 }) && (AConst > (max + BCol[i]))))
+			{
+				atomicExch(errorFlag, static_cast<int32_t>(QueryEngineError::GPU_INTEGER_OVERFLOW_ERROR));
+				continue;
+			}
+		}
+		output[i] = AConst - BCol[i];
+	}
+}
+
+/// <summary>
 /// Kernel MULTIPLICATION constant
 /// </summary>
 /// <param name="output">output result data block</param>
@@ -176,6 +207,42 @@ __global__ void kernel_floor_division_const(T *output, U *ACol, V BConst, int32_
 }
 
 /// <summary>
+/// Kernel FLOOR DIVISION (constant / column)
+/// </summary>
+/// <param name="output">output result data block</param>
+/// <param name="AConst">left input operand</param>
+/// <param name="BCol">block of the right input operands</param>
+/// <param name="dataElementCount">count of elements in the input block</param>
+/// <param name="errorFlag">flag for error checking</param>
+template<typename T, typename U, typename V>
+__global__ void kernel_floor_division_const_col(T *output, U AConst, V *BCol, int32_t dataElementCount, int32_t* errorFlag)
+{
+	int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+	int32_t stride = blockDim.x * gridDim.x;
+
+	for (int32_t i = idx; i < dataElementCount; i += stride)
+	{
+		// if none of the input operands are float
+		if (!std::is_floating_point<U>::value && !std::is_floating_point<V>::value)
+		{
+			// Check for zero division
+			if (BCol[i] == V{ 0 })
+			{
+				atomicExch(errorFlag, static_cast<int32_t>(QueryEngineError::GPU_DIVISION_BY_ZERO_ERROR));
+			}
+			else
+			{
+				output[i] = AConst / BCol[i];
+			}
+		}
+		else
+		{
+			output[i] = floorf(AConst / BCol[i]);
+		}
+	}
+}
+
+/// <summary>
 /// Kernel DIVISION constant - as T always use some kind of floating point type!
 /// </summary>
 /// <param name="output">output result data block</param>
@@ -204,6 +271,34 @@ __global__ void kernel_division_const(T *output, U *ACol, V BConst, int32_t data
 }
 
 /// <summary>
+/// Kernel DIVISION (constant / column) - as T always use some kind of floating point type!
+/// </summary>
+/// <param name="output">output result data block</param>
+/// <param name="AConst">left input operand</param>
+/// <param name="BCol">block of the right input operands</param>
+/// <param name="dataElementCount">count of elements in the input block</param>
+/// <param name="errorFlag">flag for error checking</param>
+template<typename T, typename U, typename V>
+__global__ void kernel_division_const_col(T *output, U AConst, V *BCol, int32_t dataElementCount, int32_t* errorFlag)
+{
+	int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+	int32_t stride = blockDim.x * gridDim.x;
+
+	for (int32_t i = idx; i < dataElementCount; i += stride)
+	{
+		// if none of the input operands are float
+		if (!std::is_floating_point<U>::value && !std::is_floating_point<V>::value)
+		{
+			output[i] = AConst / static_cast<T>(BCol[i]); // convert divisor to type T (should be floating point)
+		}
+		else
+		{
+			output[i] = AConst / BCol[i];
+		}
+	}
+}
+
+/// <summary>
 /// Kernel MODULO constant - as U and V never use floating point type!
 /// </summary>
 /// <param name="output">output result data block</param>
@@ -223,9 +318,35 @@ __global__ void kernel_modulo_const(T *output, U *ACol, V BConst, int32_t dataEl
 	}
 }
 
+/// <summary>
+/// Kernel MODULO (constant / column) - as U and V never use floating point type!
+/// </summary>
+/// <param name="output">output result data block</param>
+/// <param name="AConst">left input operand</param>
+/// <param name="BCol">block of the right input operands</param>
+/// <param name="dataElementCount">count of elements in the input block</param>
+/// <param name="errorFlag">flag for error checking</param>
+template<typename T, typename U, typename V>
+__global__ void kernel_modulo_const_col(T *output, U AConst, V *BCol, int32_t dataElementCount, int32_t* errorFlag)
+{
+	int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+	int32_t stride = blockDim.x * gridDim.x;
+
+	for (int32_t i = idx; i < dataElementCount; i += stride)
+	{
+		// Check for zero division
+		if (BCol[i] == V{ 0 })
+		{
+			atomicExch(errorFlag, static_cast<int32_t>(QueryEngineError::GPU_DIVISION_BY_ZERO_ERROR));
+		}
+		else
+		{
+			output[i] = AConst % BCol[i];
+		}
+	}
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 
 class GPUArithmeticConst {
 public:
@@ -251,6 +372,19 @@ public:
 			<< < Context::getInstance().calcGridDim(dataElementCount), Context::getInstance().getBlockDim() >> >
 			(output, ACol, BConst, dataElementCount, errorFlagSwapper.getFlagPointer(),
 				std::numeric_limits<T>::min() + static_cast<T>(BConst), std::numeric_limits<T>::max() + static_cast<T>(BConst));
+
+		cudaDeviceSynchronize();
+	}
+
+	template<typename T, typename U, typename V>
+	static void minus(T *output, U AConst, V* BCol, int32_t dataElementCount)
+	{
+		ErrorFlagSwapper errorFlagSwapper;
+
+		kernel_minus_const_col <T, U, V>
+			<< < Context::getInstance().calcGridDim(dataElementCount), Context::getInstance().getBlockDim() >> >
+			(output, AConst, BCol, dataElementCount, errorFlagSwapper.getFlagPointer(),
+				std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
 
 		cudaDeviceSynchronize();
 	}
@@ -289,6 +423,18 @@ public:
 	}
 
 	template<typename T, typename U, typename V>
+	static void floorDivision(T *output, U AConst, V *BCol, int32_t dataElementCount)
+	{
+		ErrorFlagSwapper errorFlagSwapper;
+
+		kernel_floor_division_const_col <T, U, V>
+			<< < Context::getInstance().calcGridDim(dataElementCount), Context::getInstance().getBlockDim() >> >
+			(output, AConst, BCol, dataElementCount, errorFlagSwapper.getFlagPointer());
+
+		cudaDeviceSynchronize();
+	}
+
+	template<typename T, typename U, typename V>
 	static void division(T *output, U *ACol, V BConst, int32_t dataElementCount)
 	{
 		// TODO Uncomment when dispatcher is ready for this
@@ -301,6 +447,23 @@ public:
 		kernel_division_const <T, U, V>
 			<< < Context::getInstance().calcGridDim(dataElementCount), Context::getInstance().getBlockDim() >> >
 			(output, ACol, BConst, dataElementCount, errorFlagSwapper.getFlagPointer());
+
+		cudaDeviceSynchronize();
+	}
+
+	template<typename T, typename U, typename V>
+	static void division(T *output, U AConst, V *BCol, int32_t dataElementCount)
+	{
+		// TODO Uncomment when dispatcher is ready for this
+		////result of this type of division operation is always floating point - so check type T
+		//static_assert(std::is_floating_point<T>::value,
+		//	"Output column of operation division has to be floating point type! For integer division use operation floorDivision.");
+
+		ErrorFlagSwapper errorFlagSwapper;
+
+		kernel_division_const_col <T, U, V>
+			<< < Context::getInstance().calcGridDim(dataElementCount), Context::getInstance().getBlockDim() >> >
+			(output, AConst, BCol, dataElementCount, errorFlagSwapper.getFlagPointer());
 
 		cudaDeviceSynchronize();
 	}
@@ -327,6 +490,22 @@ public:
 
 			cudaDeviceSynchronize();
 		}
+	}
+
+	template<typename T, typename U, typename V>
+	static void modulo(T *output, U AConst, V *BCol, int32_t dataElementCount)
+	{
+		//modulo is not defined for floating point type
+		static_assert(!std::is_floating_point<U>::value && !std::is_floating_point<V>::value,
+			"None of the input columns of operation modulo cannot be floating point type!");
+
+		ErrorFlagSwapper errorFlagSwapper;
+
+		kernel_modulo_const_col <T, U, V>
+			<< < Context::getInstance().calcGridDim(dataElementCount), Context::getInstance().getBlockDim() >> >
+			(output, AConst, BCol, dataElementCount, errorFlagSwapper.getFlagPointer());
+
+		cudaDeviceSynchronize();
 	}
 };
 
