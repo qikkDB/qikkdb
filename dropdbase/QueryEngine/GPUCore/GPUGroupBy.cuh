1457bd8e7aa790942bcdc33674bb4fd63934b1fe
Nikolas Hamran <nikolas.hamran96@gmail.com>
2019-01-28 22:26:24 +0100

    Added agg funs as template parameters for group by

--- dropdbase/QueryEngine/GPUCore/GPUGroupBy.cuh
+++ dropdbase/QueryEngine/GPUCore/GPUGroupBy.cuh
@@ -1,13 +1,42 @@
 #ifndef GPU_GROUP_BY_CUH
 #define GPU_GROUP_BY_CUH
 
+#include <cstdint>
+
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
-class GPUGroupBy {
+#include "../Context.h"
+
+template<typename AGG, typename K, typename V>
+__global__ void group_by_kernel(K *outKeys, V *outValues, int32_t outDataElementCount, K *inKeys, V *inValues, int32_t dataElementCount)
+{
+	// DEMO mock
+	outKeys[0] = AGG{}(5, 4);
+}
+
+class GPUGroupBy
+{
 public:
 
+	// Generic agg function functors
+	struct min { template<typename T> __device__ T operator()(T a, T b) const { return a < b ? a : b; } };
+	struct max { template<typename T> __device__ T operator()(T a, T b) const { return a > b ? a : b; } };
+	struct sum { template<typename T> __device__ T operator()(T a, T b) const { return a + b; } };
+	//struct avg { void operator()() const {} };
+	//struct cnt { void operator()() const {} };
+
+	template<typename AGG, typename K, typename V>
+	static void groupBy(K *outKeys, V *outValues, int32_t outDataElementCount, K *inKeys, V *inValues, int32_t dataElementCount)
+	{
+		// DEMO mock
+		group_by_kernel <AGG> << <1, 1 >> >
+			(outKeys, outValues, outDataElementCount, inKeys, inValues, dataElementCount);
+		cudaDeviceSynchronize();
+
+		Context::getInstance().getLastError().setCudaError(cudaGetLastError());
+	}
 };
 
 #endif
--- dropdbase/main.cpp
+++ dropdbase/main.cpp
@@ -1,3 +1,4 @@
+
 #include <cstdio>
 #include <iostream>
 #include <chrono>
@@ -7,32 +8,31 @@
 #include <boost/log/sinks/text_file_backend.hpp>
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/console.hpp>
+
 #include "DatabaseGenerator.h"
 #include "QueryEngine/Context.h"
 #include "QueryEngine/GPUCore/GPUMemory.cuh"
+#include "QueryEngine/GPUCore/GPUGroupBy.cuh"
 
 int main(int argc, char **argv)
 {
 	Context::getInstance(); // Initialize CUDA context
 
-    boost::log::add_file_log("../log/ColmnarDB.log");
-    boost::log::add_console_log(std::cout);
+	boost::log::add_file_log("../log/ColmnarDB.log");
+	boost::log::add_console_log(std::cout);
 
-    std::shared_ptr<Database> database = DatabaseGenerator::GenerateDatabase("TestDb", 2, 1 << 24);
+	std::shared_ptr<Database> database = DatabaseGenerator::GenerateDatabase("TestDb", 2, 1 << 24);
 	GPUMemory::hostPin(dynamic_cast<BlockBase<int32_t>&>(*dynamic_cast<ColumnBase<int32_t>&>(*(database->GetTables().at("TableA").GetColumns().at("colInteger"))).GetBlocksList()[0]).GetData().data(), 1 << 24);
 	auto start = std::chrono::high_resolution_clock::now();
+
+	GpuSqlCustomParser parser(database, "SELECT colInteger FROM TableA WHERE (colInteger + 2) * 2 <= 20;");
+	parser.parse();
+
+	auto end = std::chrono::high_resolution_clock::now();
+
+	std::chrono::duration<double> elapsed(end - start);
+
+	std::cout << "Elapsed time: " << elapsed.count() << " s\n";
 	
-
-    GpuSqlCustomParser parser(database, "SELECT colInteger FROM TableA WHERE (colInteger + 2) * 2 <= 20;");
-    parser.parse();
-
-    auto end = std::chrono::high_resolution_clock::now();
-
-    std::chrono::duration<double> elapsed(end - start);
-
-    std::cout << "Elapsed time: " << elapsed.count() << " s\n";
-
-
-
-    return 0;
+	return 0;
 }
