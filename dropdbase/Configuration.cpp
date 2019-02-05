37faed3f3592ed5be0028ec425418d47ced6e0b0
Martin Stano <martin.stano@icloud.com>
2019-02-05 10:08:43 +0100

    configuration log removal because of dependencies

--- dropdbase/Configuration.cpp
+++ dropdbase/Configuration.cpp
@@ -8,7 +8,7 @@ void Configuration::LoadConfigurationFile()
 {
 	// loading and parsing file
 	try {
-		yamlParsed_ = YAML::LoadFile(this->configurationFile);
+		yamlParsed_ = YAML::LoadFile(this->configurationFile); 
 	}
 	catch (YAML::ParserException& e) {
 		//BOOST_LOG_TRIVIAL(info) << "Configuration file could not be parsed. Using default values." << std::endl;
