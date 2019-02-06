c2740864826651cb186371843fe8a0047e6bef0b
Andrej Fusek <andrej.fusek@gmail.com>
2019-02-06 15:13:33 +0100

    Demo: Add CSV and database loading

--- dropdbase/main.cpp
+++ dropdbase/main.cpp
@@ -13,26 +13,40 @@
 #include "QueryEngine/GPUCore/GPUMemory.cuh"
 #include "ColumnBase.h"
 #include "Database.h"
+#include "CSVDataImporter.h"
 
 int main(int argc, char **argv)
 {
-	
-	std::vector<std::string> tableNames = { "TableA" };
-	std::vector<DataType> columnTypes = { {COLUMN_INT}, {COLUMN_INT}, {COLUMN_LONG}, {COLUMN_FLOAT}, {COLUMN_POLYGON}, {COLUMN_POINT} };
-	std::shared_ptr<Database> database = DatabaseGenerator::GenerateDatabase("TestDb", 2, 1 << 5, false, tableNames, columnTypes);
+	std::cout << "Start." << std::endl;
+	if (argc > 1)
+	{
+		CSVDataImporter csvDataImporter("TargetLoc.csv");
+		std::shared_ptr<Database> database = std::make_shared<Database>("TestDb", 1 << 24);
+		Database::AddToInMemoryDatabaseList(database);
+		std::cout << "Loading TargetLoc.csv ..." << std::endl;
+		csvDataImporter.ImportTables(database);
+		std::cout << "Saving dbs..." << std::endl;
+		Database::SaveAllToDisk();
+	}
+	Database::LoadDatabasesFromDisk();
+	if (Database::GetLoadedDatabases().size() == 0)
+	{
+		std::cout << "No dbs loaded, use some switch" << std::endl;
+	}
 
+	std::cout << "Parsing..." << std::endl;
 	//GPUMemory::hostPin(dynamic_cast<BlockBase<int32_t>&>(*dynamic_cast<ColumnBase<int32_t>&>(*(database->GetTables().at("TableA").GetColumns().at("colInteger"))).GetBlocksList()[0]).GetData().data(), 1 << 24);
 	auto start = std::chrono::high_resolution_clock::now();
 	
-
-    GpuSqlCustomParser parser(database, "SELECT AVG(colInteger1), colInteger2 FROM TableA GROUP BY colInteger2;");
+    GpuSqlCustomParser parser(Database::GetDatabaseByName("TestDb"), "SELECT AVG(wealthIndexId), genderId FROM TargetLoc GROUP BY genderId;");
     parser.parse();
 
     auto end = std::chrono::high_resolution_clock::now();
 
     std::chrono::duration<double> elapsed(end - start);
 
-    std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+    std::cout << "Elapsed time: " << elapsed.count()*1000 << " ms\n";
 
+	system("PAUSE");
     return 0;
 }
