fc370737b3f28da52ee14a8e33f96b1180415b3a
Martin Stano <martin.stano@icloud.com>
2019-04-11 22:21:33 +0200

    added group by regression tests

---
+++ dropdbase_test/DispatcherGroupByTests.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+
+#include "gtest/gtest.h"
+#include "../dropdbase/DatabaseGenerator.h"
+#include "../dropdbase/ColumnBase.h"
+#include "../dropdbase/BlockBase.h"
+#include "../dropdbase/PointFactory.h"
+#include "../dropdbase/ComplexPolygonFactory.h"
+#include "../dropdbase/Database.h"
+#include "../dropdbase/QueryEngine/Context.h"
+#include "../dropdbase/GpuSqlParser/GpuSqlCustomParser.h"
+#include "../dropdbase/messages/QueryResponseMessage.pb.h"
+#include "DispatcherObjs.h"
+
+TEST(DispatcherTests, GroupByEmptySet)
+{
+	Context::getInstance();
+
+	GpuSqlCustomParser parser(DispatcherObjs::GetInstance().DispatcherObjs::GetInstance().database, "SELECT COUNT(colInteger1) FROM TableA WHERE colInteger1 > 5000 GROUP BY colInteger1;");
+	auto resultPtr = parser.parse();
+	auto result = dynamic_cast<ColmnarDB::NetworkClient::Message::QueryResponseMessage*>(resultPtr.get());
+
+	std::vector<int32_t> expectedResult;
+
+}
