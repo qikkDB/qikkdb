f76209b7d30058637a0dca373655b5878940bf43
Dávid Kubík <david.kubik@instarea.com>
2019-06-04 14:46:12 +0200

    Add queries and csproj

---
+++ ColmnarDB.BenchmarkUtility/benchmark_queries.sql
@@ -0,0 +1,13 @@
+SELECT COUNT(ageId) FROM TargetLoc1B WHERE latitude > 48.1303 AND latitude < 48.1391 AND longitude > 17.0779 AND longitude < 17.0912 GROUP BY ageId;
+SELECT COUNT(ageId) FROM TargetLoc1B WHERE latitude > 48.1395 AND latitude < 48.1573 AND longitude > 17.0798 AND longitude < 17.0952 GROUP BY ageId;
+SELECT COUNT(ageId) FROM TargetLoc1B WHERE latitude > 48.1415 AND latitude < 48.1534 AND longitude > 17.0894 AND longitude < 17.1027 GROUP BY ageId;
+SELECT COUNT(ageId) FROM TargetLoc1B WHERE latitude > 48.1512 AND latitude < 48.1612 AND longitude > 17.0996 AND longitude < 17.1126 GROUP BY ageId;
+SELECT COUNT(ageId) FROM TargetLoc1B WHERE latitude > 48.1549 AND latitude < 48.1708 AND longitude > 17.1033 AND longitude < 17.1193 GROUP BY ageId;
+SELECT COUNT(ageId) FROM TargetLoc1B WHERE latitude > 48.1592 AND latitude < 48.1785 AND longitude > 17.1116 AND longitude < 17.1312 GROUP BY ageId;
+SELECT COUNT(ageId) FROM TargetLoc1B WHERE latitude > 48.1689 AND latitude < 48.1799 AND longitude > 17.1202 AND longitude < 17.1388 GROUP BY ageId;
+SELECT COUNT(ageId) FROM TargetLoc1B WHERE latitude > 48.179  AND latitude < 48.1977 AND longitude > 17.1218 AND longitude < 17.1397 GROUP BY ageId;
+SELECT COUNT(ageId) FROM TargetLoc1B WHERE latitude > 48.1868 AND latitude < 48.1991 AND longitude > 17.1299 AND longitude < 17.1411 GROUP BY ageId;
+SELECT COUNT(ageId) FROM TargetLoc1B WHERE latitude > 48.1946 AND latitude < 48.2142 AND longitude > 17.1386 AND longitude < 17.1512 GROUP BY ageId;
+SELECT targetId, SUM(ageId) as sum_age, sUm(ageId * (1 - targetId)) as something1, sum(ageId * (1 - targetId) * (1 + genderId)) as something2, avg(ageId) as avg_age, count(targetId) as count_ids FROM TargetLoc1B WHERE hwOsId <= 3 group by genderId;
+SELECT longitude, latitude, sum(wealthIndexId) AS sum_wealthIndexId FROM TargetLoc1B WHERE (latitude - longitude) < 35;
+SELECT min(longitude), max(latitude) FROM TargetLoc1B;
