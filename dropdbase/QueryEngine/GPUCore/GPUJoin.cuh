acd3b985c3df685f6758e7da5d8f800e03be58b8
Nikolas Hamran <nikolas.hamran96@gmail.com>
2019-06-10 16:18:04 +0200

    WIP join, work in progress

--- dropdbase/QueryEngine/GPUCore/GPUJoin.cuh
+++ dropdbase/QueryEngine/GPUCore/GPUJoin.cuh
@@ -9,12 +9,19 @@
 
 #include "../Context.h"
 #include "GPUMemory.cuh"
+#include "../../../cub/cub.cuh"
 
-__device__ const int32_t HASH_BUCKET_COUNT = 1024;
+__device__ const int32_t HASH_BUCKET_COUNT = 1024; // Context::getInstance().getBlockDim();
+
+__device__ int32_t hash(int32_t key)
+{
+    return key % HASH_BUCKET_COUNT;
+}
 
 template <typename T>
-__global__ void kernel_calc_histo(T* RCol, int32_t* hashTableHisto, int32_t dataElementCount)
+__global__ void kernel_calc_histo(T* RCol, int32_t* hashTableHisto_, int32_t dataElementCount)
 {
+    // Shared memory for the local hash value histograms
     __shared__ int32_t shared_mem[HASH_BUCKET_COUNT];
 
     const int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -22,60 +29,167 @@ __global__ void kernel_calc_histo(T* RCol, int32_t* hashTableHisto, int32_t data
 
     for (int32_t i = idx; i < dataElementCount; i += stride)
     {
+        // Set all local memory locations to zero and wait for each other
+        shared_mem[threadIdx.x] = 0;
+        __syncthreads();
+
+        // Hash the values of the input rows and atomically increment the counters in the local memory
+        atomicAdd(&shared_mem[hash(RCol[i])], 1);
+
+        // Wait for each other again and write the results, so the local memory is free for the next batch
+        __syncthreads();
+        hashTableHisto_[i] = shared_mem[threadIdx.x];
     }
 }
+
+/*
+template <typename T>
+__global__ void kernel_sort_local(T* RCol,
+								  int32_t* hashTableHisto_,
+                                  int32_t* hashTablePrefixSum_, 
+								  int32_t* hashTableSortedId_, 
+								  int32_t dataElementCount)
+{
+    // Shared memory for the local hash value histograms
+    __shared__ int32_t shared_mem[HASH_BUCKET_COUNT];
+
+    const int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    const int32_t stride = blockDim.x * gridDim.x;
+
+    for (int32_t i = idx; i < dataElementCount; i += stride)
+    {
+        // Load the histograms into memory
+        shared_mem[threadIdx.x] = hashTablePrefixSum_[i];
+        __syncthreads();
+
+        // Sort the id's of the entries based on the histogram
+        for (int32_t j = 0; j < hashTableHisto_[i]; j++)
+		{
+            int32_t old_idx = atomicAdd(&shared_mem[threadIdx.x], 1);
+            hashTableSortedId_[old_idx] = threadIdx.x;
+		}
+    }
+}*/
 
 class GPUJoin
 {
 public:
     // Constructor, requires the number of GPU blocks and their size
-    GPUJoin(int32_t hashTableBlockCount, int32_t hashTableBlockDataElementCount)
-    : hashTableBlockCount_(hashTableBlockCount),
-      hashTableBlockDataElementCount_(hashTableBlockDataElementCount),
-      hashTableHisto_(hashTableBlockCount), hashTablePrefixSum_(hashTableBlockCount),
-      hashTableId_(hashTableBlockCount)
+    GPUJoin(int32_t hashTableBlockCount, int32_t hashTableBlockSize)
+    : hashTableBlockCount_(hashTableBlockCount), 
+	  hashTableBlockSize_(hashTableBlockSize), 
+	  hashTableTotalSize_(hashTableBlockCount_ * hashTableBlockSize_),
+	  hashTableHisto_(nullptr),
+      hashTablePrefixSum_(nullptr), 
+	  hashTableSortedId_(nullptr)
     {
         // Alloc the hash table for each block
-        for (int32_t i = 0; i < hashTableBlockCount_; i++)
-        {
-            GPUMemory::alloc(&hashTableHisto_[i], hashTableBlockDataElementCount_);
-            GPUMemory::alloc(&hashTablePrefixSum_[i], hashTableBlockDataElementCount_);
-            GPUMemory::alloc(&hashTableId_[i], hashTableBlockDataElementCount_);
-        }
+        GPUMemory::alloc(&hashTableHisto_, hashTableTotalSize_);
+        GPUMemory::alloc(&hashTablePrefixSum_, hashTableTotalSize_);
+        GPUMemory::alloc(&hashTableSortedId_, hashTableTotalSize_);
     }
 
     ~GPUJoin()
     {
         // Dealloc the hash table for each block
-        for (int32_t i = 0; i < hashTableBlockCount_; i++)
-        {
-            GPUMemory::free(hashTableHisto_[i]);
-            GPUMemory::free(hashTablePrefixSum_[i]);
-            GPUMemory::free(hashTableId_[i]);
-        }
+        GPUMemory::free(hashTableHisto_);
+        GPUMemory::free(hashTablePrefixSum_);
+        GPUMemory::free(hashTableSortedId_);
     }
 
     // Construct a hash table histogram and prefix sum per block and keep it on the GPU
     template <typename T>
-    void HashBlock(T* RCol, int32_t blockOrder, int32_t dataElementCount)
+    void CalcBlockHashHisto(T* RCol, int32_t blockOrder)
     {
-        int32_t* processed_block = hashTableHisto_[blockOrder];
+        if (blockOrder < 0 || blockOrder >= hashTableBlockCount_)
+        {
+            std::cerr << "[ERROR] Hash data block order index out of range" << std::endl;
+        }
 
-        kernel_calc_histo<<<Context::getInstance().calcGridDim(dataElementCount),
-                            Context::getInstance().getBlockDim()>>>(RCol, processed_block, dataElementCount);
+		// Calculate the hash histograms
+        int32_t* processed_block = &hashTableHisto_[blockOrder * hashTableBlockSize_];
+        kernel_calc_histo<<<Context::getInstance().calcGridDim(hashTableBlockSize_),
+                            Context::getInstance().getBlockDim()>>>(RCol, processed_block, hashTableBlockSize_);
     }
 
+	void CalcGlobalPrefixSum()
+    {
+        // Calculate the exclusive prefix sum of the hash table histograms
+        void* tempBuffer = nullptr;
+        size_t tempBufferSize = 0;
+
+        // Calculate the prefix sum
+        // in-place scan
+        cub::DeviceScan::InclusiveSum(tempBuffer, tempBufferSize, hashTableHisto_, hashTablePrefixSum_,
+                                      hashTableBlockCount_ * hashTableBlockSize_);
+        
+		// Allocate temporary storage
+        GPUMemory::alloc<int8_t>(reinterpret_cast<int8_t**>(&tempBuffer), tempBufferSize);
+        
+		// Run exclusive prefix sum
+        cub::DeviceScan::ExclusiveSum(tempBuffer, tempBufferSize, hashTableHisto_, hashTablePrefixSum_,
+                                      hashTableBlockCount_ * hashTableBlockSize_);
+        GPUMemory::free(tempBuffer);
+	}
+
 	template <typename T>
+	void SortHashTableIDs(T* RCol, int32_t blockOrder)
+    {
+        /*
+		if (blockOrder < 0 || blockOrder >= hashTableBlockCount_)
+        {
+            std::cerr << "[ERROR] Hash data block order index out of range" << std::endl;
+        }
+
+		// Sort the row indices in blocks locally
+        int32_t* processed_block_histo = &hashTableHisto_[blockOrder * hashTableBlockSize_];
+        int32_t* processed_block_prefix_sum = &hashTablePrefixSum_[blockOrder * hashTableBlockSize_];
+        int32_t* processed_block_sorted_id = &hashTableSortedId_[blockOrder * hashTableBlockSize_];
+
+		kernel_sort_local<<<Context::getInstance().calcGridDim(hashTableBlockSize_),
+                            Context::getInstance().getBlockDim()>>>(RCol, 
+																	processed_block_histo,
+																	processed_block_prefix_sum,
+																	processed_block_sorted_id,
+                                                                    hashTableBlockSize_);
+																	*/
+	}
+
+	void PrintDebug()
+    {
+        // DEBUG - Print the intermediate results
+        int32_t* hashTableHisto_data = new int32_t[hashTableBlockCount_ * hashTableBlockSize_];
+        int32_t* hashTablePrefixSum_data = new int32_t[hashTableBlockCount_ * hashTableBlockSize_];
+        int32_t* hashTableSortedId_data = new int32_t[hashTableBlockCount_ * hashTableBlockSize_];
+
+        GPUMemory::copyDeviceToHost(hashTableHisto_data, hashTableHisto_, hashTableBlockCount_ * hashTableBlockSize_);
+        GPUMemory::copyDeviceToHost(hashTablePrefixSum_data, hashTablePrefixSum_,
+                                    hashTableBlockCount_ * hashTableBlockSize_);
+        GPUMemory::copyDeviceToHost(hashTableSortedId_data, hashTableSortedId_,
+                                    hashTableBlockCount_ * hashTableBlockSize_);
+
+        for (int32_t i = 0; i < hashTableBlockCount_ * hashTableBlockSize_; i++)
+        {
+            std::printf("%5d %5d %5d %5d\n", i % hashTableBlockSize_, hashTableHisto_data[i],
+                        hashTablePrefixSum_data[i], hashTableSortedId_data[i]);
+        }
+
+		delete[] hashTableHisto_data;
+        delete[] hashTablePrefixSum_data;
+        delete[] hashTableSortedId_data;
+	}
+
+    template <typename T>
     void JoinBlock(T* SCol, int32_t dataElementCount)
     {
-
     }
 
 private:
     int32_t hashTableBlockCount_;
-    int32_t hashTableBlockDataElementCount_;
+    int32_t hashTableBlockSize_;
+    int32_t hashTableTotalSize_;
 
-    std::vector<int32_t*> hashTableHisto_;
-    std::vector<int32_t*> hashTablePrefixSum_;
-    std::vector<int32_t*> hashTableId_;
+    int32_t* hashTableHisto_;
+    int32_t* hashTablePrefixSum_;
+    int32_t* hashTableSortedId_;
 };
--- dropdbase_test/GPUJoinTests.cu
+++ dropdbase_test/GPUJoinTests.cu
@@ -5,5 +5,42 @@ TEST(GPUJoinTests, JoinTest)
 {
     // Initialize CUDA context:
     Context::getInstance();
+   
+	// Alloc buffers
+    const int32_t blockCount = 2;		// Block count can be inf (GPU limits)
+    const int32_t blockSize = 8;		// block size can be inf (GPU limits)
+    int32_t fk[blockCount][blockSize] = {
+		{5, 1, 0, 2, 0, 2, 1, 7},
+		{0, 2, 5, 0, 1, 1, 7, 0}
+	};
 
+	// Alloc a feed buffer for building the hash table block - wise
+    int32_t* d_feed_block;
+    GPUMemory::alloc(&d_feed_block, blockSize);
+
+	// Create a join instance to hold the hash table
+	GPUJoin gpuJoin(blockCount, blockSize);
+
+	// Copy the input block-wise and build the join hash table
+    for (int32_t i = 0; i < blockCount; i++)
+    {
+		GPUMemory::copyHostToDevice(d_feed_block, fk[i], blockSize);
+        gpuJoin.CalcBlockHashHisto(d_feed_block, i);
+	}
+
+	gpuJoin.CalcGlobalPrefixSum();
+
+	/*
+	for (int32_t i = 0; i < blockCount; i++)
+    {
+        GPUMemory::copyHostToDevice(d_feed_block, fk[i], blockSize);
+        gpuJoin.SortHashTableIDs(d_feed_block, i);
+    }
+	*/
+
+	gpuJoin.PrintDebug();
+
+	GPUMemory::free(d_feed_block);
+
+	ASSERT_EQ(false, true);
 }
