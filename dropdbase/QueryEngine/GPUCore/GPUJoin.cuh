9e3b2fae5db8e8b777faa13bc7a114ce1247a0c3
Nikolas Hamran <nikolas.hamran96@gmail.com>
2019-06-11 11:27:38 +0200

    WIP started join again, fixed block collision issues and data ordering

--- dropdbase/QueryEngine/GPUCore/GPUJoin.cuh
+++ dropdbase/QueryEngine/GPUCore/GPUJoin.cuh
@@ -5,191 +5,152 @@
 #include <device_launch_parameters.h>
 
 #include <cstdint>
-#include <vector>
-
-#include "../Context.h"
-#include "GPUMemory.cuh"
 #include "../../../cub/cub.cuh"
 
-__device__ const int32_t HASH_BUCKET_COUNT = 1024; // Context::getInstance().getBlockDim();
+#include "GPUMemory.cuh"
+#include "GPUArithmetic.cuh"
 
-__device__ int32_t hash(int32_t key)
-{
-    return key % HASH_BUCKET_COUNT;
-}
-
+// Kernel - Calculate the hashed value occurances - histogram of hashes
 template <typename T>
-__global__ void kernel_calc_histo(T* RCol, int32_t* hashTableHisto_, int32_t dataElementCount)
+__global__ void kernel_calc_hash_histo(int32_t* currentBlockHisto, T* tableBlock, int32_t blockSize)
 {
-    // Shared memory for the local hash value histograms
-    __shared__ int32_t shared_mem[HASH_BUCKET_COUNT];
-
     const int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
     const int32_t stride = blockDim.x * gridDim.x;
 
-    for (int32_t i = idx; i < dataElementCount; i += stride)
+    for (int32_t i = idx; i < blockSize; i += stride)
     {
-        // Set all local memory locations to zero and wait for each other
-        shared_mem[threadIdx.x] = 0;
-        __syncthreads();
-
-        // Hash the values of the input rows and atomically increment the counters in the local memory
-        atomicAdd(&shared_mem[hash(RCol[i])], 1);
-
-        // Wait for each other again and write the results, so the local memory is free for the next batch
-        __syncthreads();
-        hashTableHisto_[i] = shared_mem[threadIdx.x];
+        int32_t hash = tableBlock[i] % blockSize;
+        atomicAdd(&currentBlockHisto[hash], 1);
     }
 }
 
-/*
+// Kernel - save the keys to a list ( into buckets) for later hash checking to avoid conflicts
 template <typename T>
-__global__ void kernel_sort_local(T* RCol,
-								  int32_t* hashTableHisto_,
-                                  int32_t* hashTablePrefixSum_, 
-								  int32_t* hashTableSortedId_, 
-								  int32_t dataElementCount)
+__global__ void kernel_save_data_to_buckets(int32_t* currentBlockBuckets,
+											int32_t* currentBlockHisto, 
+											int32_t* currentBlockPrefixSum,
+                                            T* tableBlock,
+                                            int32_t blockSize)
 {
-    // Shared memory for the local hash value histograms
-    __shared__ int32_t shared_mem[HASH_BUCKET_COUNT];
-
     const int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
     const int32_t stride = blockDim.x * gridDim.x;
 
-    for (int32_t i = idx; i < dataElementCount; i += stride)
+    for (int32_t i = idx; i < blockSize; i += stride)
     {
-        // Load the histograms into memory
-        shared_mem[threadIdx.x] = hashTablePrefixSum_[i];
-        __syncthreads();
-
-        // Sort the id's of the entries based on the histogram
-        for (int32_t j = 0; j < hashTableHisto_[i]; j++)
-		{
-            int32_t old_idx = atomicAdd(&shared_mem[threadIdx.x], 1);
-            hashTableSortedId_[old_idx] = threadIdx.x;
-		}
+        int32_t hash = tableBlock[i] % blockSize;
+        int32_t bucket_index = atomicAdd(&currentBlockPrefixSum[hash], 1);
+        currentBlockBuckets[bucket_index] = tableBlock[i];
     }
-}*/
+}
 
 class GPUJoin
 {
+private:
+    int32_t joinTableBlockCount_;
+    int32_t joinTableBlockSize_;
+    int32_t joinTableTotalSize_;
+
+	int32_t* joinTableHashHistoTable_;
+    int32_t* joinTableHashPrefixSum_;
+    int32_t* joinTableHashTableBuckets_;
+
 public:
-    // Constructor, requires the number of GPU blocks and their size
-    GPUJoin(int32_t hashTableBlockCount, int32_t hashTableBlockSize)
-    : hashTableBlockCount_(hashTableBlockCount), 
-	  hashTableBlockSize_(hashTableBlockSize), 
-	  hashTableTotalSize_(hashTableBlockCount_ * hashTableBlockSize_),
-	  hashTableHisto_(nullptr),
-      hashTablePrefixSum_(nullptr), 
-	  hashTableSortedId_(nullptr)
+    GPUJoin(int32_t joinTableBlockCount, int32_t joinTableBlockSize) : 
+		joinTableBlockCount_(joinTableBlockCount), 
+		joinTableBlockSize_(joinTableBlockSize), 
+		joinTableTotalSize_(joinTableBlockCount * joinTableBlockSize)
     {
-        // Alloc the hash table for each block
-        GPUMemory::alloc(&hashTableHisto_, hashTableTotalSize_);
-        GPUMemory::alloc(&hashTablePrefixSum_, hashTableTotalSize_);
-        GPUMemory::alloc(&hashTableSortedId_, hashTableTotalSize_);
-    }
+        GPUMemory::alloc(&joinTableHashHistoTable_, joinTableTotalSize_);
+        GPUMemory::alloc(&joinTableHashPrefixSum_, joinTableTotalSize_);
+        GPUMemory::alloc(&joinTableHashTableBuckets_, joinTableTotalSize_);
+	}
 
-    ~GPUJoin()
+	~GPUJoin()
     {
-        // Dealloc the hash table for each block
-        GPUMemory::free(hashTableHisto_);
-        GPUMemory::free(hashTablePrefixSum_);
-        GPUMemory::free(hashTableSortedId_);
-    }
+        GPUMemory::free(joinTableHashHistoTable_);
+        GPUMemory::free(joinTableHashPrefixSum_);
+        GPUMemory::free(joinTableHashTableBuckets_);
+	}
 
-    // Construct a hash table histogram and prefix sum per block and keep it on the GPU
-    template <typename T>
-    void CalcBlockHashHisto(T* RCol, int32_t blockOrder)
-    {
-        if (blockOrder < 0 || blockOrder >= hashTableBlockCount_)
-        {
-            std::cerr << "[ERROR] Hash data block order index out of range" << std::endl;
-        }
+	// Hash the first table into memory
+	template<typename T>
+	void HashBlock(T* tableBlock, int32_t blockOrder)
+	{
+		// Check if the chosen block is in range
+		if (blockOrder < 0 || blockOrder >= joinTableBlockCount_)
+		{
+            std::cerr << "[ERROR] Block id out of limits during block hashing" << std::endl;
+		}
 
-		// Calculate the hash histograms
-        int32_t* processed_block = &hashTableHisto_[blockOrder * hashTableBlockSize_];
-        kernel_calc_histo<<<Context::getInstance().calcGridDim(hashTableBlockSize_),
-                            Context::getInstance().getBlockDim()>>>(RCol, processed_block, hashTableBlockSize_);
-    }
+		/////////////////////////////////////////////////////////////////////////////////////////
+		// Calculate the histogram of hashes
+		int32_t* currentBlockHisto = &joinTableHashHistoTable_[blockOrder * joinTableBlockSize_];
+        kernel_calc_hash_histo<<<Context::getInstance().calcGridDim(joinTableBlockSize_),
+                                 Context::getInstance().getBlockDim()>>>(currentBlockHisto, 
+																		 tableBlock,
+                                                                         joinTableBlockSize_);
 
-	void CalcGlobalPrefixSum()
-    {
-        // Calculate the exclusive prefix sum of the hash table histograms
+		/////////////////////////////////////////////////////////////////////////////////////////
+		// Calculate the prefix sum for this block
+        int32_t* currentBlockPrefixSum = &joinTableHashPrefixSum_[blockOrder * joinTableBlockSize_];
+
         void* tempBuffer = nullptr;
         size_t tempBufferSize = 0;
-
         // Calculate the prefix sum
         // in-place scan
-        cub::DeviceScan::InclusiveSum(tempBuffer, tempBufferSize, hashTableHisto_, hashTablePrefixSum_,
-                                      hashTableBlockCount_ * hashTableBlockSize_);
-        
-		// Allocate temporary storage
+        cub::DeviceScan::ExclusiveSum(tempBuffer, tempBufferSize, currentBlockHisto,
+                                      currentBlockPrefixSum, joinTableBlockSize_);
+        // Allocate temporary storage
         GPUMemory::alloc<int8_t>(reinterpret_cast<int8_t**>(&tempBuffer), tempBufferSize);
-        
-		// Run exclusive prefix sum
-        cub::DeviceScan::ExclusiveSum(tempBuffer, tempBufferSize, hashTableHisto_, hashTablePrefixSum_,
-                                      hashTableBlockCount_ * hashTableBlockSize_);
+        // Run exclusive prefix sum
+        cub::DeviceScan::ExclusiveSum(tempBuffer, tempBufferSize, currentBlockHisto,
+                                      currentBlockPrefixSum, joinTableBlockSize_);
         GPUMemory::free(tempBuffer);
+
+		/////////////////////////////////////////////////////////////////////////////////////////
+		// Save the data to buckets for later hash collision resolution
+        int32_t* currentBlockBuckets = &joinTableHashTableBuckets_[blockOrder * joinTableBlockSize_];
+		kernel_save_data_to_buckets<<<Context::getInstance().calcGridDim(joinTableBlockSize_),
+                                      Context::getInstance().getBlockDim()>>>(currentBlockBuckets, 
+																			  currentBlockHisto,
+                                                                              currentBlockPrefixSum, 
+																			  tableBlock,
+                                                                              joinTableBlockSize_);
+		// Restore the prefix sum buffer
+		// IMPORTANT - the minus restores only those elements which have a nonzero occurance in the
+		// histogram table !, but that is ok, because if an element has 0 occurances in this
+		// table, it is not present and therefore no join is possible with this element
+        GPUArithmetic::colCol<ArithmeticOperations::sub>(currentBlockPrefixSum,
+                                                         currentBlockPrefixSum, 
+														 currentBlockHisto, 
+														 joinTableBlockSize_);
 	}
 
 	template <typename T>
-	void SortHashTableIDs(T* RCol, int32_t blockOrder)
-    {
-        /*
-		if (blockOrder < 0 || blockOrder >= hashTableBlockCount_)
-        {
-            std::cerr << "[ERROR] Hash data block order index out of range" << std::endl;
-        }
+	void JoinBlock(T* joinBlock)
+	{
 
-		// Sort the row indices in blocks locally
-        int32_t* processed_block_histo = &hashTableHisto_[blockOrder * hashTableBlockSize_];
-        int32_t* processed_block_prefix_sum = &hashTablePrefixSum_[blockOrder * hashTableBlockSize_];
-        int32_t* processed_block_sorted_id = &hashTableSortedId_[blockOrder * hashTableBlockSize_];
-
-		kernel_sort_local<<<Context::getInstance().calcGridDim(hashTableBlockSize_),
-                            Context::getInstance().getBlockDim()>>>(RCol, 
-																	processed_block_histo,
-																	processed_block_prefix_sum,
-																	processed_block_sorted_id,
-                                                                    hashTableBlockSize_);
-																	*/
 	}
 
-	void PrintDebug()
-    {
-        // DEBUG - Print the intermediate results
-        int32_t* hashTableHisto_data = new int32_t[hashTableBlockCount_ * hashTableBlockSize_];
-        int32_t* hashTablePrefixSum_data = new int32_t[hashTableBlockCount_ * hashTableBlockSize_];
-        int32_t* hashTableSortedId_data = new int32_t[hashTableBlockCount_ * hashTableBlockSize_];
+	void debugInfo()
+	{
+        int32_t* host_joinTableHashHistoTable = new int32_t[joinTableTotalSize_];
+        int32_t* host_joinTableHashPrefixSum = new int32_t[joinTableTotalSize_];
+        int32_t* host_joinTableHashTableBuckets = new int32_t[joinTableTotalSize_];
 
-        GPUMemory::copyDeviceToHost(hashTableHisto_data, hashTableHisto_, hashTableBlockCount_ * hashTableBlockSize_);
-        GPUMemory::copyDeviceToHost(hashTablePrefixSum_data, hashTablePrefixSum_,
-                                    hashTableBlockCount_ * hashTableBlockSize_);
-        GPUMemory::copyDeviceToHost(hashTableSortedId_data, hashTableSortedId_,
-                                    hashTableBlockCount_ * hashTableBlockSize_);
+		GPUMemory::copyDeviceToHost(host_joinTableHashHistoTable, joinTableHashHistoTable_, joinTableTotalSize_);
+        GPUMemory::copyDeviceToHost(host_joinTableHashPrefixSum, joinTableHashPrefixSum_, joinTableTotalSize_);
+        GPUMemory::copyDeviceToHost(host_joinTableHashTableBuckets, joinTableHashTableBuckets_, joinTableTotalSize_);
 
-        for (int32_t i = 0; i < hashTableBlockCount_ * hashTableBlockSize_; i++)
-        {
-            std::printf("%5d %5d %5d %5d\n", i % hashTableBlockSize_, hashTableHisto_data[i],
-                        hashTablePrefixSum_data[i], hashTableSortedId_data[i]);
-        }
+		for (int32_t i = 0; i < joinTableTotalSize_; i++)
+		{
+            std::printf("%d %d %d\n",
+				host_joinTableHashHistoTable[i], host_joinTableHashPrefixSum[i], host_joinTableHashTableBuckets[i]);
+		}
+		
+		delete[] host_joinTableHashHistoTable;
+        delete[] host_joinTableHashPrefixSum;
+        delete[] host_joinTableHashTableBuckets;
 
-		delete[] hashTableHisto_data;
-        delete[] hashTablePrefixSum_data;
-        delete[] hashTableSortedId_data;
 	}
-
-    template <typename T>
-    void JoinBlock(T* SCol, int32_t dataElementCount)
-    {
-    }
-
-private:
-    int32_t hashTableBlockCount_;
-    int32_t hashTableBlockSize_;
-    int32_t hashTableTotalSize_;
-
-    int32_t* hashTableHisto_;
-    int32_t* hashTablePrefixSum_;
-    int32_t* hashTableSortedId_;
 };
--- dropdbase_test/GPUJoinTests.cu
+++ dropdbase_test/GPUJoinTests.cu
@@ -1,46 +1,28 @@
-#include "gtest/gtest.h"
 #include "../dropdbase/QueryEngine/GPUCore/GPUJoin.cuh"
+#include "../dropdbase/QueryEngine/GPUCore/GPUMemory.cuh"
+#include "gtest/gtest.h"
 
 TEST(GPUJoinTests, JoinTest)
 {
-    // Initialize CUDA context:
-    Context::getInstance();
-   
-	// Alloc buffers
-    const int32_t blockCount = 2;		// Block count can be inf (GPU limits)
-    const int32_t blockSize = 8;		// block size can be inf (GPU limits)
-    int32_t fk[blockCount][blockSize] = {
-		{5, 1, 0, 2, 0, 2, 1, 7},
-		{0, 2, 5, 0, 1, 1, 7, 0}
-	};
+    const int32_t tableBlockCount = 1;
+    const int32_t tableBlockSize = 16;
+    const int32_t tableTotalSize = tableBlockSize * tableBlockCount;
 
-	// Alloc a feed buffer for building the hash table block - wise
-    int32_t* d_feed_block;
-    GPUMemory::alloc(&d_feed_block, blockSize);
+    int32_t table[tableTotalSize] = {5, 1, 0, 2, 0, 2, 1, 7, 0, 2, 5, 0, 1, 1, 7, 0};
 
-	// Create a join instance to hold the hash table
-	GPUJoin gpuJoin(blockCount, blockSize);
+    int32_t* d_table;
+    GPUMemory::alloc(&d_table, tableTotalSize);
+    GPUMemory::copyHostToDevice(d_table, table, tableTotalSize);
 
-	// Copy the input block-wise and build the join hash table
-    for (int32_t i = 0; i < blockCount; i++)
+    GPUJoin gpuJoin(tableBlockCount, tableBlockSize);
+    for (int32_t i = 0; i < tableBlockCount; i++)
     {
-		GPUMemory::copyHostToDevice(d_feed_block, fk[i], blockSize);
-        gpuJoin.CalcBlockHashHisto(d_feed_block, i);
+        gpuJoin.HashBlock(&d_table[i * tableBlockSize], i);
 	}
 
-	gpuJoin.CalcGlobalPrefixSum();
+	gpuJoin.debugInfo();
 
-	/*
-	for (int32_t i = 0; i < blockCount; i++)
-    {
-        GPUMemory::copyHostToDevice(d_feed_block, fk[i], blockSize);
-        gpuJoin.SortHashTableIDs(d_feed_block, i);
-    }
-	*/
+    GPUMemory::free(d_table);
 
-	gpuJoin.PrintDebug();
-
-	GPUMemory::free(d_feed_block);
-
-	ASSERT_EQ(false, true);
+    ASSERT_EQ(false, true);
 }
