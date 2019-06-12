b21fda011467e760a037a26ab0aad227797ea7a8
Dávid Kubík <david.kubik@instarea.com>
2019-06-12 10:51:15 +0200

    Change used database for testing, do not import database anymore

--- ColmnarDB.BenchmarkUtility/Program.cs
+++ ColmnarDB.BenchmarkUtility/Program.cs
@@ -14,12 +14,10 @@ namespace ColmnarDB.BenchmarkUtility
         public static readonly string resultFilePath = "results.txt";
         public static readonly int numberOfQueryExec = 5;
 
-        public static readonly string telcoDataCsvPath = "test-data/TargetLoc1B.csv";
-        public static readonly string telcoDbName = "TargetLoc1B";
+        public static readonly string telcoDbName = "mlddb";
         public static readonly string telcoQueriesPath = "ColmnarDB.BenchmarkUtility/telco_queries.sql";
 
-        public static readonly string geoDataCsvPath = "test-data/zones335.csv";
-        public static readonly string geoDbName = "zones335";
+        public static readonly string geoDbName = "GeoTest";
         public static readonly string geoQueriesPath = "ColmnarDB.BenchmarkUtility/geo_queries.sql";
 
         /// <summary>
@@ -31,6 +29,8 @@ namespace ColmnarDB.BenchmarkUtility
             client.Connect();
             Console.Out.WriteLine("Client has successfully connected to server.");
 
+            UseDatabase use = new UseDatabase();
+
             if (File.Exists(resultFilePath))
             {
                 File.Delete(resultFilePath);
@@ -39,18 +39,10 @@ namespace ColmnarDB.BenchmarkUtility
             var resultFile = new System.IO.StreamWriter(resultFilePath);
 
             //test telco queries:
-            Console.Out.WriteLine("Importing CSV file named: " + telcoDataCsvPath + ". Please wait.");
-            var start = DateTime.Now;
-            client.ImportCSV(telcoDbName, telcoDataCsvPath);
-            var timeDiff = DateTime.Now - start;
-            Console.Out.WriteLine("Importing of CSV file has successfully finished.");
-
+            use.Use(telcoDbName, client);
             resultFile.WriteLine("Each query was executed " + numberOfQueryExec + " times and the average time was saved.");
-            resultFile.WriteLine("Importing CSV file (" + telcoDataCsvPath + ") took: " + timeDiff.ToString() + " (HH:MM:SS).");
-
+            
             client.UseDatabase(telcoDbName);
-
-            Console.Out.WriteLine("Database was successfully loaded.");
 
             var queryFile = new System.IO.StreamReader(telcoQueriesPath);
             Console.Out.WriteLine("Benchmark queries from file '" + telcoQueriesPath + "' were loaded.");
@@ -67,12 +59,7 @@ namespace ColmnarDB.BenchmarkUtility
                 (Dictionary<string, List<object>> queryResult, Dictionary<string, float> executionTimes) result = (null, null);
                 result = client.GetNextQueryResult();
                 float resultSum = result.executionTimes.Values.Sum();
-
-                foreach (var key in result.executionTimes.Keys)
-                {
-                    Console.Out.WriteLine("Execution times key: " + key);
-                }
-                
+                Console.Out.WriteLine("Execution time was: " + result.executionTimes.Values.Sum());
                 
                 //save query result to a file:
                 resultFile.WriteLine((resultSum).ToString() + " (first run)");
@@ -93,17 +80,10 @@ namespace ColmnarDB.BenchmarkUtility
             queryFile.Close();
 
             //test geo queries:
-            Console.Out.WriteLine("Starting importing CSV file named: " + geoDataCsvPath);
-            start = DateTime.Now;
-            client.ImportCSV(geoDbName, geoDataCsvPath);
-            timeDiff = DateTime.Now - start;
-            Console.Out.WriteLine("Importing of CSV file has successfully finished.");
-
-            resultFile.WriteLine("Importing CSV file (" + geoDataCsvPath + ") took: " + timeDiff.ToString() + " (HH:MM:SS).");
-
+            use.Use(geoDbName, client);
+            resultFile.WriteLine("Each query was executed " + numberOfQueryExec + " times and the average time was saved.");
+            
             client.UseDatabase(geoDbName);
-
-            Console.Out.WriteLine("Database was successfully loaded.");
 
             queryFile = new System.IO.StreamReader(geoQueriesPath);
             Console.Out.WriteLine("Benchmark queries from file '" + geoQueriesPath + "' were loaded.");
@@ -137,13 +117,6 @@ namespace ColmnarDB.BenchmarkUtility
             }
             queryFile.Close();
             resultFile.Close();
-
-            //drop databases:
-            queryString = "DROP DATABASE " + telcoDbName + ";";
-            client.Query(queryString);
-
-            queryString = "DROP DATABASE " + geoDbName + ";";
-            client.Query(queryString);
         }    
     }
 }
