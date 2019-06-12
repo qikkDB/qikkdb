ccfa2efd21dfd9f5a5bae6e48c1d3dfe204b9582
Dávid Kubík <david.kubik@instarea.com>
2019-06-12 10:55:58 +0200

    Change query

--- ColmnarDB.BenchmarkUtility/telco_queries.sql
+++ ColmnarDB.BenchmarkUtility/telco_queries.sql
@@ -8,5 +8,5 @@ SELECT COUNT(ageId) FROM TargetLoc1B WHERE latitude > 48.1689 AND latitude < 48.
 SELECT COUNT(ageId) FROM TargetLoc1B WHERE latitude > 48.179  AND latitude < 48.1977 AND longitude > 17.1218 AND longitude < 17.1397 GROUP BY ageId;
 SELECT COUNT(ageId) FROM TargetLoc1B WHERE latitude > 48.1868 AND latitude < 48.1991 AND longitude > 17.1299 AND longitude < 17.1411 GROUP BY ageId;
 SELECT COUNT(ageId) FROM TargetLoc1B WHERE latitude > 48.1946 AND latitude < 48.2142 AND longitude > 17.1386 AND longitude < 17.1512 GROUP BY ageId;
-SELECT longitude, latitude, sum(wealthIndexId) AS sum_wealthIndexId FROM TargetLoc1B WHERE (latitude - longitude) < 35;
+SELECT sum(wealthIndexId) AS sum_wealthIndexId FROM TargetLoc1B WHERE (latitude - longitude) < 15;
 SELECT min(longitude), max(latitude) FROM TargetLoc1B;
