4631bf713a45c3d6a7d7f3edd0ad7dc4212d8346
Martin Stano <martin.stano@icloud.com>
2019-06-25 11:54:10 +0200

    debug outputs

--- dropdbase/GpuSqlParser/GpuSqlJoinFunctions.h
+++ dropdbase/GpuSqlParser/GpuSqlJoinFunctions.h
@@ -33,8 +33,22 @@ int32_t GpuSqlJoinDispatcher::joinCol()
 	{
 	case JoinType::INNER_JOIN:
 		GPUJoin::JoinTableRonS<OP, T>(leftJoinIndices, rightJoinIndices, *colBaseLeft, *colBaseRight, database->GetBlockSize());
-		joinIndices.emplace(leftTable, std::move(leftJoinIndices));
-		joinIndices.emplace(rightTable, std::move(rightJoinIndices));
+		//joinIndices.emplace(leftTable, std::move(leftJoinIndices));
+		//joinIndices.emplace(rightTable, std::move(rightJoinIndices));
+
+		std::cout << leftTable << std::endl;
+		for (int32_t i = 0; i < leftJoinIndices.size(); i++)
+		{
+			for (int32_t j = 0; j < leftJoinIndices[i].size(); j++)
+			{
+				T dataLeft = dynamic_cast<BlockBase<T>*>(dynamic_cast<ColumnBase<T>*>(database->GetTables().at(leftTable).GetColumns().at(leftColumn).get())->GetBlocksList()[leftJoinIndices[i][j] / database->GetBlockSize()])->GetData()[leftJoinIndices[i][j] % database->GetBlockSize()];
+				T dataRight = dynamic_cast<BlockBase<T>*>(dynamic_cast<ColumnBase<T>*>(database->GetTables().at(rightTable).GetColumns().at(rightColumn).get())->GetBlocksList()[rightJoinIndices[i][j] / database->GetBlockSize()])->GetData()[rightJoinIndices[i][j] % database->GetBlockSize()];
+				std::cout << leftJoinIndices[i][j] << " " << dataLeft << std::endl;
+				std::cout << rightJoinIndices[i][j] << " " << dataRight << std::endl;
+				std::cout << std::endl;
+			}
+		}
+		
 		break;
 	case JoinType::LEFT_JOIN:
 	case JoinType::RIGHT_JOIN:
