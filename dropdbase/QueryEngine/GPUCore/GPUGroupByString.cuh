f3dbc62d881341eb63d2f0230e40fad5ea1f978c
Andrej Fusek <andrej.fusek@instarea.com>
2019-06-25 17:36:10 +0200

    GroupByString: Add multiGPU support, WIP

--- dropdbase/QueryEngine/GPUCore/GPUGroupByString.cuh
+++ dropdbase/QueryEngine/GPUCore/GPUGroupByString.cuh
@@ -10,38 +10,18 @@ constexpr int32_t GBS_SOURCE_INDEX_KEY_IN_BUFFER = -2;
 constexpr int32_t GBS_STRING_HASH_SEED = 31;
 
 
-__device__ int32_t GetHash(char* text, int32_t length)
-{
-    int32_t hash = 0;
-    for (int32_t i = 0; i < length; i++)
-    {
-        hash = GBS_STRING_HASH_SEED * hash + text[i];
-    }
-    return hash;
-}
+__device__ int32_t GetHash(char* text, int32_t length);
 
 
-__device__ bool AreEqualStrings(char* textA, int32_t lenghtA, GPUMemory::GPUString stringColB, int64_t indexB)
-{
-    return FilterConditions::equal{}.compareStrings(textA, lenghtA,
-                                                    stringColB.allChars +
-                                                        GetStringIndex(stringColB.stringIndices, indexB),
-                                                    GetStringLength(stringColB.stringIndices, indexB));
-}
+__device__ bool AreEqualStrings(char* textA, int32_t lenghtA, GPUMemory::GPUString stringColB, int64_t indexB);
 
 
 __device__ bool IsNewKey(char* checkedKeyChars,
-                         int32_t checkedKeyLength,
-                         GPUMemory::GPUString inKeys,
-                         GPUMemory::GPUString keysBuffer,
-                         int32_t* sourceIndices,
-                         int32_t index)
-{
-    return (sourceIndices[index] >= 0 &&
-            !AreEqualStrings(checkedKeyChars, checkedKeyLength, inKeys, sourceIndices[index])) || // find in inKeys at first
-           (sourceIndices[index] == GBS_SOURCE_INDEX_KEY_IN_BUFFER &&
-            !AreEqualStrings(checkedKeyChars, checkedKeyLength, keysBuffer, index)); // and in keysBuffer next
-}
+	int32_t checkedKeyLength,
+	GPUMemory::GPUString inKeys,
+	GPUMemory::GPUString keysBuffer,
+	int32_t* sourceIndices,
+	int32_t index);
 
 
 /// GROUP BY Kernel processes input (inKeys and inValues). New keys from inKeys are added
@@ -137,63 +117,18 @@ __global__ void kernel_group_by_string(int32_t* sourceIndices,
 
 
 __global__ void kernel_collect_string_keys(GPUMemory::GPUString sideBuffer,
-                                           int32_t* sourceIndices,
-                                           int32_t* stringLengths,
-                                           GPUMemory::GPUString keysBuffer,
-                                           int32_t maxHashCount,
-                                           GPUMemory::GPUString inKeys,
-                                           int32_t inKeysCount)
-{
-    const int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
-    const int32_t stride = blockDim.x * gridDim.x;
-
-    for (int32_t i = idx; i < maxHashCount; i += stride)
-    {
-        if (sourceIndices[i] >= 0)
-        {
-            for (int32_t j = 0; j < stringLengths[i]; j++)
-            {
-                sideBuffer.allChars[GetStringIndex(sideBuffer.stringIndices, i) + j] =
-                    inKeys.allChars[GetStringIndex(inKeys.stringIndices, sourceIndices[i]) + j];
-            }
-        }
-        else if (sourceIndices[i] == GBS_SOURCE_INDEX_KEY_IN_BUFFER)
-        {
-            for (int32_t j = 0; j < stringLengths[i]; j++)
-            {
-                sideBuffer.allChars[GetStringIndex(sideBuffer.stringIndices, i) + j] =
-                    keysBuffer.allChars[GetStringIndex(keysBuffer.stringIndices, i) + j];
-            }
-        }
-    }
-}
+	int32_t* sourceIndices,
+	int32_t* stringLengths,
+	GPUMemory::GPUString keysBuffer,
+	int32_t maxHashCount,
+	GPUMemory::GPUString inKeys,
+	int32_t inKeysCount);
 
 
-__global__ void kernel_is_bucket_occupied(int8_t* occupancyMask, int32_t* sourceIndices, int32_t maxHashCount)
-{
-    const int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
-    const int32_t stride = blockDim.x * gridDim.x;
-
-    for (int32_t i = idx; i < maxHashCount; i += stride)
-    {
-        occupancyMask[i] = (sourceIndices[i] != GBS_SOURCE_INDEX_EMPTY_KEY);
-    }
-}
+__global__ void kernel_is_bucket_occupied(int8_t* occupancyMask, int32_t* sourceIndices, int32_t maxHashCount);
 
 
-__global__ void kernel_mark_collected_strings(int32_t* sourceIndices, int32_t maxHashCount)
-{
-    const int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
-    const int32_t stride = blockDim.x * gridDim.x;
-
-    for (int32_t i = idx; i < maxHashCount; i += stride)
-    {
-        if (sourceIndices[i] >= 0)
-        {
-            sourceIndices[i] = GBS_SOURCE_INDEX_KEY_IN_BUFFER;
-        }
-    }
-}
+__global__ void kernel_mark_collected_strings(int32_t* sourceIndices, int32_t maxHashCount);
 
 
 /// GROUP BY generic class (for MIN, MAX and SUM).
@@ -331,6 +266,33 @@ public:
     }
 
 
+    /// Reconstruct needed raw fields (do not calculate final results yet)
+    /// Reconstruct keys, values and key occurence counts separately
+    /// <param name="keys">output buffer to fill with reconstructed keys</param>
+    /// <param name="values">output buffer to fill with reconstructed values</param>
+    /// <param name="occurences">not used buffer if using operations MIN, MAX or SUM - nullptr can be used</param>
+    /// <param name="elementCount">ouptut buffer to fill with element count (one int32_t number)</param>
+    void reconstructRawNumbers(std::vector<int32_t>& keysStringLengths,
+                               std::vector<char>& keysAllChars,
+                               V* values,
+                               int64_t* occurences,
+                               int32_t* elementCount)
+    {
+        Context& context = Context::getInstance();
+        cuda_ptr<int8_t> occupancyMask(maxHashCount_, 0);
+        kernel_is_bucket_occupied<<<context.calcGridDim(maxHashCount_), context.getBlockDim()>>>(
+            occupancyMask.get(), sourceIndices_, maxHashCount_);
+
+        GPUReconstruct::ReconstructStringColRaw(keysStringLengths, keysAllChars, elementCount,
+                                                keys_, occupancyMask.get(), maxHashCount_);
+        GPUReconstruct::reconstructCol(values, elementCount, values_, occupancyMask.get(), maxHashCount_);
+    }
+
+
+    /// Get the final results of GROUP BY operation - for operations Min, Max and Sum on single GPU
+    /// <param name="outKeys">pointer to GPUString struct (will be allocated and filled with final keys)</param>
+    /// <param name="outValues">double pointer of output GPU buffer (will be allocated and filled with final values)</param>
+    /// <param name="outDataElementCount">output CPU buffer (will be filled with count of reconstructed elements)</param>
     void getResults(GPUMemory::GPUString* outKeys, O** outValues, int32_t* outDataElementCount)
     {
         Context& context = Context::getInstance();
@@ -341,5 +303,95 @@ public:
                                                  occupancyMask.get(), maxHashCount_);
         GPUReconstruct::reconstructColKeep(outValues, outDataElementCount, values_,
                                            occupancyMask.get(), maxHashCount_);
+    }
+
+
+    /// Merge results from all devices and store to buffers on default device (multi GPU function)
+    /// <param name="outKeys">pointer to GPUString struct (will be allocated and filled with final
+    /// keys)</param> <param name="outValues">double pointer of output GPU buffer (will be allocated
+    /// and filled with final values)</param> <param name="outDataElementCount">output CPU buffer
+    /// (will be filled with count of reconstructed elements)</param> <param name="tables">vector of
+    /// unique pointers to IGroupBy objects with hash tables on every device (GPU)</param>
+    void getResults(GPUMemory::GPUString* outKeys,
+                    O** outValues,
+                    int32_t* outDataElementCount,
+                    std::vector<std::unique_ptr<IGroupBy>>& tables)
+    {
+        if (tables.size() <= 0) // invalid count of tables
+        {
+            throw std::invalid_argument("Number of tables have to be at least 1.");
+        }
+        else if (tables.size() == 1 || tables[1].get() == nullptr) // just one table
+        {
+            getResults(outKeys, outValues, outDataElementCount);
+        }
+        else // more tables
+        {
+            int oldDeviceId = Context::getInstance().getBoundDeviceID();
+
+            std::vector<int32_t> keysAllHostStringLengths;
+            std::vector<char> keysAllHostAllChars;
+            std::vector<V> valuesAllHost;
+            int32_t sumElementCount = 0;
+
+            // Collect data from all devices (graphic cards) to host
+            for (int i = 0; i < tables.size(); i++)
+            {
+                if (tables[i].get() == nullptr)
+                {
+                    break;
+                }
+                // TODO change to cudaMemcpyPeerAsync
+                GPUGroupBy<AGG, O, GPUMemory::GPUString, V>* table =
+                    reinterpret_cast<GPUGroupBy<AGG, O, GPUMemory::GPUString, V>*>(tables[i].get());
+                std::vector<int32_t> keysStringLengths;
+                std::vector<char> keysAllChars;
+                std::unique_ptr<V[]> values = std::make_unique<V[]>(table->getMaxHashCount());
+                int32_t elementCount;
+                Context::getInstance().bindDeviceToContext(i);
+
+                // Reconstruct keys and values
+                table->reconstructRawNumbers(keysStringLengths, keysAllChars, values.get(), nullptr, &elementCount);
+
+                // Append data to host vectors
+                keysAllHostStringLengths.insert(keysAllHostStringLengths.end(),
+                                                keysStringLengths.begin(), keysStringLengths.end());
+                keysAllHostAllChars.insert(keysAllHostAllChars.end(), keysAllChars.begin(),
+                                           keysAllChars.end());
+                valuesAllHost.insert(valuesAllHost.end(), values.get(), values.get() + elementCount);
+                sumElementCount += elementCount;
+            }
+
+            Context::getInstance().bindDeviceToContext(oldDeviceId);
+            if (sumElementCount > 0)
+            {
+                cuda_ptr<int32_t> keysAllGPUStringLengths(sumElementCount);
+                cuda_ptr<V> valuesAllGPU(sumElementCount);
+
+                // Copy the condens from host to default device
+                GPUMemory::copyHostToDevice(keysAllGPUStringLengths.get(),
+                                            keysAllHostStringLengths.data(), sumElementCount);
+                GPUMemory::copyHostToDevice(valuesAllGPU.get(), valuesAllHost.data(), sumElementCount);
+
+                // Construct new GPUString
+                GPUMemory::GPUString keysAllGPU;
+                GPUMemory::alloc(&(keysAllGPU.stringIndices), sumElementCount);
+                GPUReconstruct::PrefixSum(keysAllGPU.stringIndices, keysAllGPUStringLengths.get(), sumElementCount);
+                GPUMemory::alloc(&(keysAllGPU.allChars), keysAllHostAllChars.size());
+                GPUMemory::copyHostToDevice(keysAllGPU.allChars, keysAllHostAllChars.data(),
+                                            keysAllHostAllChars.size());
+
+                // Merge results
+                GPUGroupBy<AGG, O, GPUMemory::GPUString, V> finalGroupBy(sumElementCount);
+                finalGroupBy.groupBy(keysAllGPU, valuesAllGPU.get(), sumElementCount);
+                finalGroupBy.getResults(outKeys, outValues, outDataElementCount);
+
+                GPUMemory::free(keysAllGPU);
+            }
+            else
+            {
+                *outDataElementCount = 0;
+            }
+        }
     }
 };
