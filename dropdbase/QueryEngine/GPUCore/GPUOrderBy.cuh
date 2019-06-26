2e97847d73910a79b7f9fc801595a6b2a1af0b88
Nikolas Hamran <nikolas.hamran@instarea.com>
2019-06-26 14:56:06 +0200

    Reorder by idx kernel added WIP

--- dropdbase/QueryEngine/GPUCore/GPUOrderBy.cuh
+++ dropdbase/QueryEngine/GPUCore/GPUOrderBy.cuh
@@ -98,6 +98,19 @@ __global__ kernel_radix_sort(int32_t* indicesOut,
 	}
 }
 
+// Reorder a column by a given index column
+template<typename T>
+__global__ void kernel_reorder_by_idx(T* outCol, int32_t* inIndices, T* inCol, int32_t dataElementCount)
+{
+    const int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+	const int32_t stride = blockDim.x * gridDim.x;
+
+	for (int32_t i = idx; i < dataElementCount; i += stride)
+	{
+        outCol[inIndices[i]] = inCol[i];
+    }
+}
+
 template<typename T>
 class GPUOrderBy {
 private:
@@ -260,6 +273,9 @@ public:
     
     void ReOrderByIdx(T* outCol, int32_t* inIndices, T* inCol, int32_t dataElementCount)
     {
-
+        // Reorder a column based on indices
+        kernel_reorder_by_idx<<< Context::getInstance().calcGridDim(dataElementCount), 
+                                 Context::getInstance().getBlockDim() >>>
+                                 (outCol, inIndices, inCol, dataElementCount);
     }
 };
