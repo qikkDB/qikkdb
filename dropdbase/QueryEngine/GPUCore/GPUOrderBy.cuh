262d038c33e91d1c447f3a3f5dbce4733bb532c4
Nikolas Hamran <nikolas.hamran@instarea.com>
2019-06-26 13:24:50 +0200

    Set up the helper structures around order by algorithm and all the helepr kernels

--- dropdbase/QueryEngine/GPUCore/GPUOrderBy.cuh
+++ dropdbase/QueryEngine/GPUCore/GPUOrderBy.cuh
@@ -1,126 +1,219 @@
 #pragma once
 
-#include "GPUMemory.cuh"
-#include "cuda_ptr.h"
-
-#include "../../../cub/cub.cuh"
-
-// Constants for radix sort
-__host__ __device__ constexpr RADIX_MASK = 0xFFFF;
-__host__ __device__ constexpr RADIX_BUCKET_COUNT = 65536;
-
-// Initialize the index buffer for reshuffling based on order by operation during the radix sort
-__global__ void kernel_init_idx_buffer(int32_t* radix_index_buffer, int32_t dataElementCount)
+// Fill the index buffers with default indices
+__global__ kernel_fill_indices(int32_t* indices, int32_t dataElementCount)
 {
     const int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
 	const int32_t stride = blockDim.x * gridDim.x;
 
 	for (int32_t i = idx; i < dataElementCount; i += stride)
 	{
-        radix_index_buffer[i] = i;
+        indices[i] = i;
 	}
 }
 
-// Perform the index rotation - by 16 bits
+// Reorder the keys and the values
+__host__ __device__ constexpr uint64_t RADIX_BUCKET_COUNT = 65536;
+__host__ __device__ constexpr uint64_t RADIX_MASK = 0xFFFF;
+__host__ __device__ constexpr uint64_t RADIX_MASK_BIT_WIDTH = 16;
+
+// Calculate the histogram of occurrances of keys (like in counting sort)
 template<typename T>
-__global__ 
-void kernel_order_by(int32_t* radix_bucket_histo, T* inCol, int32_t dataElementCount, uint32_t radixMaskShift)
+__global__ kernel_radix_histo(int32_t* radix_histo, 
+                              T* keys,
+                              int32_t radix_pass,
+                              bool is_signed_pass,
+                              int32_t dataElementCount)
 {
     const int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
 	const int32_t stride = blockDim.x * gridDim.x;
 
 	for (int32_t i = idx; i < dataElementCount; i += stride)
 	{
-        uint32_t radixShiftVal = static_cast<uint32_t>(inCol[i]);
-
-        int32_t radixIndex = (radixShiftVal & (RADIX_MASK << radixMaskShift)) >> radixMaskShift;
-        atomicAdd(&radix_bucket_histo[radixIndex], 1);
+        int32_t radixHistoIdx = (keys[i] & (RADIX_MASK << (RADIX_MASK_BIT_WIDTH * radix_pass))) >> 
+                                (RADIX_MASK_BIT_WIDTH * radix_pass);
+        atomicAdd(&radix_histo[radixHistoIdx], 1);
 	}
 }
 
-// Reorder the rotated indices based on the prefix sum
+// Reorder the indices and the keys based on the radix and data type
 template<typename T>
-__global__
-void kernel_reorder_by_idx()
+__global__ kernel_radix_sort(int32_t* indicesOut, 
+                             T* keysOut,
+                             int32_t* indicesIn,
+                             T* keysIn,
+                             int32_t* radix_pref_sum,
+                             int32_t radix_pass,
+                             bool is_signed_pass,
+                             int32_t dataElementCount)
 {
+    const int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+	const int32_t stride = blockDim.x * gridDim.x;
 
+	for (int32_t i = idx; i < dataElementCount; i += stride)
+	{
+        
+	}
 }
 
 template<typename T>
 class GPUOrderBy {
 private:
-    int32_t* radix_bucket_histo_;
+    // Radix indices front and back buffer
+    int32_t* indices1;
+    int32_t* indices2;
 
-    size_t radix_bucket_prefix_sum_size_;
-    int8_t* radix_bucket_prefix_sum_temp_;
-    int32_t* radix_bucket_prefix_sum_;
+    // Keys front and back buffer
+    T* keys1;
+    T* keys2;
 
-    int32_t* radix_index_buffer_1_;
-    int32_t* radix_index_buffer_2_;
+    // Radix key histogram and prefix sum buffer
+    int32_t* radix_histo_;
+    int32_t* radix_pref_sum_;
 
-    T* radix_buffer_1_;
-    T* radix_buffer_2_;
+    // Prefix sum helper buffers
+    size_t radix_pref_sum_temp_buf_size_;
+    int8_t* radix_pref_sum_temp_buf_;
+
+    // One iteration of the radix sort algorithm put in one function
+    void RadixPass(int32_t* indicesOut, 
+                   T* keysOut,
+                   int32_t* indicesIn,
+                   T* keysIn,
+                   int32_t radix_pass,
+                   bool is_signed_pass,
+                   int32_t dataElementCount) {
+        // Zero the histogram
+        GPUMemory::fillArray(radix_histo_, 0, RADIX_BUCKET_COUNT);
+
+        // Calcualte the radix histogram
+        kernel_radix_histo<<< Context::getInstance().calcGridDim(dataElementCount), 
+                            Context::getInstance().getBlockDim() >>>
+                            (radix_histo_, 
+                            keysIn, 
+                            radix_pass, 
+                            is_signed_pass, 
+                            dataElementCount);
+
+        // Calcualte the histogram inclusive prefix sum
+        cub::DeviceScan::InclusiveSum(radix_pref_sum_temp_buf_, 
+                                    radix_pref_sum_temp_buf_size_, 
+                                    radix_histo_, 
+                                    radix_pref_sum_, 
+                                    dataElementCount);
+
+        // Perform the radix sort on the keys, order the indices with the keys too
+        kernel_radix_sort<<< Context::getInstance().calcGridDim(dataElementCount), 
+                            Context::getInstance().getBlockDim() >>>
+                            (indicesOut, 
+                            keysOut,
+                            indicesIn,
+                            keysIn,
+                            radix_pref_sum_temp_buf_, 
+                            radix_pass, 
+                            is_signed_pass, 
+                            dataElementCount);
+    }
     
 public:
     GPUOrderBy(int32_t dataElementCount)
     {
-        GPUMemory::alloc(&radix_bucket_histo_, RADIX_BUCKET_COUNT);
-        GPUMemory::alloc(&radix_bucket_prefix_sum_, RADIX_BUCKET_COUNT);
+        GPUMemoryAlloc(&indices1, dataElementCount);
+        GPUMemoryAlloc(&indices2, dataElementCount);
+        GPUMemoryAlloc(&keys1, dataElementCount);
+        GPUMemoryAlloc(&keys2, dataElementCount);
 
-        // Preallocate a helper buffer for the prefix sum
-        radix_bucket_prefix_sum_size_ = 0;
-		cub::DeviceScan::InclusiveSum(nullptr, radix_bucket_prefix_sum_size_, radix_bucket_histo_, radix_bucket_prefix_sum_, RADIX_BUCKET_COUNT);
-		GPUMemory::alloc(&radix_bucket_prefix_sum_temp_, radix_bucket_prefix_sum_size_);
-        
-        GPUMemory::alloc(&radix_index_buffer_1_, dataElementCount);
-        GPUMemory::alloc(&radix_index_buffer_2_, dataElementCount);
-        GPUMemory::alloc(&radix_buffer_1_, dataElementCount);
-        GPUMemory::alloc(&radix_buffer_2_, dataElementCount);
+        GPUMemoryAlloc(&radix_histo_, RADIX_BUCKET_COUNT);
+        GPUMemoryAlloc(&radix_pref_sum_, RADIX_BUCKET_COUNT);
 
-        // Fill the first index buffer with the default indices
-        kernel_init_idx_buffer<<<Context::getInstance().calcGridDim(dataElementCount), 
-                                 Context::getInstance().getBlockDim()>>>
-                                 (radix_index_buffer_, dataElementCount);
+        radix_pref_sum_temp_buf_size_ = 0;
+        cub::DeviceScan::InclusiveSum(nullptr, 
+                                      radix_pref_sum_temp_buf_size_, 
+                                      radix_histo_, 
+                                      radix_pref_sum_, 
+                                      dataElementCount);
+        GPUMemoryAlloc(&radix_pref_sum_temp_buf_, radix_pref_sum_temp_buf_size_);
     }
 
     ~GPUOrderBy()
     {
-        GPUMemory::free(radix_bucket_histo_);
-        GPUMemory::free(radix_bucket_prefix_sum_);
-        
-        GPUMemory::free(radix_bucket_prefix_sum_temp_);
+        GPUMemoryFree(indices1);
+        GPUMemoryFree(indices2);
+        GPUMemoryFree(keys1);
+        GPUMemoryFree(keys2);
 
-        GPUMemory::free(radix_index_buffer_1_);
-        GPUMemory::free(radix_index_buffer_2_);
-        GPUMemory::free(radix_buffer_1_);
-        GPUMemory::free(radix_buffer_2_);
+        GPUMemoryFree(radix_histo_);
+        GPUMemoryFree(radix_pref_sum_);
+        GPUMemoryFree(radix_pref_sum_temp_buf_);
     }
 
     void OrderBy(int32_t* outColIndices, std::vector<T*> &inCols, int32_t dataElementCount)
     {
-        for(int32_t i = inCols.size() - 1; i >= 0; i--)
+        // FInitialize the index buffer
+        kernel_fill_indices<<< Context::getInstance().calcGridDim(dataElementCount), 
+                               Context::getInstance().getBlockDim() >>>
+                               (indices1, dataElementCount);
+
+        // Iterate trough all the columns and sort them with radix sort
+        // Handle the columns as if they were a big binary number from right to left
+        for(int32_t i = 0; i < inCols.size()l i++)
         {
-            // Zero the histogram and the radix buffers
-            GPUMemory::fillArray(radix_bucket_histo_, 0, dataElementCount);
-            GPUMemory::fillArray(radix_buffer_1_, 0, dataElementCount);
-            GPUMemory::fillArray(radix_buffer_2_, 0, dataElementCount);
+            // Copy the keys to the first key buffer
+            GPUMemory::copyDeviceToDevice(keys1, inCols[i], dataElementCount)
 
-            // Count the radix occurrances then reorder the indices 
-            kernel_order_by<<<Context::getInstance().calcGridDim(dataElementCount), 
-                              Context::getInstance().getBlockDim()>>>
-                              (radix_bucket_histo_, &inCols[i], dataElementCount, 0);
+            // Passes for different data types
+            if(std::is_same<T, int32_t>::value || std::is_same<T, float>::value )
+            {
+                // Signed 32 bit integers or 32 bit float values
+                // First pass - unsigned lower 16 bits
+                RadixPass(indices2, keys2, indices1, keys1, 0, false, dataElementCount);
 
-            // Calculate the prefix sum for the histogram
-            cub::DeviceScan::InclusiveSum(radix_bucket_prefix_sum_temp_, radix_bucket_prefix_sum_size_, radix_bucket_histo_, radix_bucket_prefix_sum_, RADIX_BUCKET_COUNT);
-            
-            // Reorder the input keys partially by the radix
-            
+                // Second pass - signed upper 16 bits
+                RadixPass(indices1, keys1, indices2, keys2, 1, true, dataElementCount);
+            } 
+            else if(std::is_same<T, uint32_t>::value)
+            {
+                // Unsigned 32 bit integers
+                // First pass - unsigned lower 16 bits
+                RadixPass(indices2, keys2, indices1, keys1, 0, false, dataElementCount);
 
+                // Second pass - signed upper 16 bits
+                RadixPass(indices1, keys1, indices2, keys2, 1, false, dataElementCount);
+            }
+            else if(std::is_same<T, int64_t>::value || std::is_same<T, double>::value )
+            {
+                // Signed 64 bit integers or 64 bit double values
+                // First pass - unsigned lower lower 16 bits
+                RadixPass(indices2, keys2, indices1, keys1, 0, false, dataElementCount);
+
+                // Second pass - signed lower upper 16 bits
+                RadixPass(indices1, keys1, indices2, keys2, 1, false, dataElementCount);
+
+                // Third pass - unsigned upper lower 16 bits
+                RadixPass(indices2, keys2, indices1, keys1, 2, false, dataElementCount);
+
+                // Fourth pass - signed upper upper 16 bits
+                RadixPass(indices1, keys1, indices2, keys2, 3, true, dataElementCount);
+            }
+            else if(std::is_same<T, int64_t>::value)
+            {
+                // Unsigned 64 bit integers
+                // First pass - unsigned lower lower 16 bits
+                RadixPass(indices2, keys2, indices1, keys1, 0, false, dataElementCount);
+
+                // Second pass - signed lower upper 16 bits
+                RadixPass(indices1, keys1, indices2, keys2, 1, false, dataElementCount);
+
+                // Third pass - unsigned upper lower 16 bits
+                RadixPass(indices2, keys2, indices1, keys1, 2, false, dataElementCount);
+
+                // Fourth pass - signed upper upper 16 bits
+                RadixPass(indices1, keys1, indices2, keys2, 3, false, dataElementCount);
+            }
         }
     }
     
-    
-    void ReorderByIdx(T* outCol, int32_t* inColIndices, T* inCol, int32_t dataElementCount)
+    void ReOrderByIdx(T* outCol, int32_t* inColIndices, T* inCol, int32_t dataElementCount)
     {
 
     }
--- dropdbase_test/GPUOrderByTests.cu
+++ dropdbase_test/GPUOrderByTests.cu
@@ -1,41 +1,6 @@
 #include "gtest/gtest.h"
-#include "../dropdbase/QueryEngine/Context.h"
-#include "../dropdbase/QueryEngine/GPUCore/GPUMemory.cuh"
-#include "../dropdbase/QueryEngine/GPUCore/GPUOrderBy.cuh"
 
 TEST(GPUOrderByTests, GPUOrderByTest)
 {
-    // Input data
-    constexpr int32_t GROUP_BY_BLOCK_COUNT = 2;
-    constexpr int32_t DATA_ELEMENT_COUNT = 16;
-    int32_t inCols[GROUP_BY_BLOCK_COUNT][DATA_ELEMENT_COUNT] = {
-        {34, 12, 34, 2, 78, 3, 34, 89, 78, 59, 12, 78, 90, 33, 78, 90}, 
-        {21, 11, 49, 25, 8, 32, 71, 82, 0, 29, 11, 64, 4, 67, 22, 91}
-    };
 
-    // Alloc the input blocks and move the data
-    std::vector<int32_t*> d_inCols(GROUP_BY_BLOCK_COUNT);
-    for(int i = 0; i < GROUP_BY_BLOCK_COUNT; i++)
-    {
-        GPUMemory::alloc(&inCols[i], DATA_ELEMENT_COUNT);
-        GPUMemory::copyHostToDevice(d_inCols[i], inCols[i], DATA_ELEMENT_COUNT);
-    }
-
-    // Result order by indices
-    int32_t* outColIndices;
-    GPUMemory::alloc(&outColIndices, DATA_ELEMENT_COUNT);
-
-    //////////////////////////////////////////////////////////////////////////////
-    // Run the order by operation
-
-    GPUOrderBy<int32_t> orderBy(DATA_ELEMENT_COUNT);
-    orderBy.OrderBy(outColIndices, d_inCols, DATA_ELEMENT_COUNT);
-
-    //////////////////////////////////////////////////////////////////////////////
-    // Dealloc the input blocks
-    for(int i = 0; i < GROUP_BY_BLOCK_COUNT; i++)
-    {
-        GPUMemory::free(d_inCols[i]);
-    }
-    GPUMemory::free(outColIndices);
 }
