5cfc0c4aeb957d333e90961a54882dc65a83fdef
David Kubik <david.kubik@instarea.com>
2019-06-27 09:16:37 +0200

    WIP

--- ColmnarDB.BenchmarkUtility/taxi_queries.sql
+++ ColmnarDB.BenchmarkUtility/taxi_queries.sql
@@ -1,4 +1,4 @@
-SELECT cab_type, count(*) FROM trips GROUP BY cab_type;
-SELECT passenger_count, avg(total_amount) FROM trips GROUP BY passenger_count;
-SELECT passenger_count, extract(year from pickup_datetime) AS pickup_year, count(*) FROM trips GROUP BY passenger_count, pickup_year;	 
-SELECT passenger_count, extract(year from pickup_datetime) AS pickup_year, cast(trip_distance as int) AS distance, count(*) AS the_count FROM trips GROUP BY passenger_count, pickup_year, distance ORDER BY pickup_year, the_count desc;
+SELECT cab_type, COUNT(*) FROM trips GROUP BY cab_type;
+SELECT passenger_count, AVG(total_amount) FROM trips GROUP BY passenger_count;
+SELECT passenger_count, YEAR(pickup_datetime) AS pickup_year, COUNT(*) FROM trips GROUP BY passenger_count, pickup_year;	 
+SELECT passenger_count, YEAR(pickup_datetime) AS pickup_year, cast(trip_distance as int) AS distance, COUNT(*) AS the_count FROM trips GROUP BY passenger_count, pickup_year, distance ORDER BY pickup_year, the_count DESC;
