9ce197782b05e5c9e12ade98208492c23b8a558c
Nikolas Hamran <nikolas.hamran@instarea.com>
2019-06-28 10:59:10 +0200

    Finished basic OrderBy test

--- dropdbase_test/GPUOrderByTests.cu
+++ dropdbase_test/GPUOrderByTests.cu
@@ -8,6 +8,32 @@
 #include <iostream>
 #include <functional>
 
+
+template<typename T>
+struct IdxKeyPair
+{
+    int32_t index;
+    T key;
+};
+
+template<typename T>
+struct Asc
+{
+    inline bool operator() (const IdxKeyPair<T>& struct1, const IdxKeyPair<T>& struct2)
+    {
+        return (struct1.key < struct2.key);
+    }
+};
+
+template<typename T>
+struct Desc
+{
+    inline bool operator() (const IdxKeyPair<T>& struct1, const IdxKeyPair<T>& struct2)
+    {
+        return (struct1.key > struct2.key);
+    }
+};
+
 TEST(GPUOrderByTests, GPUOrderByUnsignedTest)
 {
     // Random generator
@@ -18,7 +44,7 @@ TEST(GPUOrderByTests, GPUOrderByUnsignedTest)
     int32_t COL_COUNT = 6;
     int32_t COL_DATA_ELEMENT_COUNT = 16;
 
-    uint32_t DATA_LIMIT = 10;
+    uint32_t NUMERIC_DATA_LIMIT = 10;
 
     // Input data
     std::vector<OrderBy::Order> orderingIn;
@@ -27,62 +53,89 @@ TEST(GPUOrderByTests, GPUOrderByUnsignedTest)
     // Fill the input data vectors
     for(int32_t i = 0; i < COL_COUNT; i++)
     {
-        orderingIn.push_back(OrderBy::Order::ASC);
+        orderingIn.push_back((rand() % 2) == 0 ? OrderBy::Order::ASC : OrderBy::Order::DESC);
         dataIn.push_back(std::vector<uint32_t>{});
         for(int32_t j = 0; j < COL_DATA_ELEMENT_COUNT; j++)
         {
-            dataIn[i].push_back(rand() % DATA_LIMIT);
+            dataIn[i].push_back(rand() % NUMERIC_DATA_LIMIT);
         }
     }
 
     /////////////////////////////////////////////////////////////////////////////
     // Sort the input data on the CPU
-    // Fill in the index buffer
-    std::vector<int32_t> indexBuffer(COL_DATA_ELEMENT_COUNT);
+    // This is done by a new algorithm where we want to sort the input columns accross
+    // multiple columns - ORDER BY
+    // The algorithm:
+    //      Input: A list of columns to be ordered by in that order
+    //      Output: A list of indices for the reordered column elements
+    //
+    //      1. Initialize a list of indices from 0 .. n where n is the number of entries in the input columns
+    //      2. Iterate over the columns from the last to the first (important to keep this order), for each column
+    //      3. Reorder the column entries into a new index, data pair, each index data pair stores an index and the
+    //         corresponding data entry (where the index points) from the input column
+    //      4. Sort the index, data pairs based on the data
+    //      5. Keep the new index combination for the next iteration
+    //      6. If all collumns are processed exit, else go to 3
+    //      7. The final indices list is the list of lexicographicsal ordering of all vectors
+    //         reorder the input collumns based on these indices to get the order by operation over all columns
+
+
+    // 0. Create the temporary sort buffers
+    std::vector<int32_t> indices(COL_DATA_ELEMENT_COUNT);
+    std::vector<uint32_t> data(COL_DATA_ELEMENT_COUNT);
+
+    // 1. Fill in the indices with the default value
     for(int32_t i = 0; i < COL_DATA_ELEMENT_COUNT; i++)
     {
-        indexBuffer[i] = i;
+        indices[i] = i;
     }
 
-    // Perform the column sorting
-    std::vector<std::pair<int32_t, uint32_t>> IKpairs(COL_DATA_ELEMENT_COUNT); 
+    // 2. Perform the column sorting from the last to the first column
     for(int32_t i = COL_COUNT - 1; i >= 0; i--)
     {
-        // Fill a vector of index - value pair and sort it based on the indices
-        
+        // 3. Reorder the column entries based on the indices
         for(int32_t j = 0; j < COL_DATA_ELEMENT_COUNT; j++)
         {
-            IKpairs[j] = std::make_pair(dataIn[i][indexBuffer[j]], indexBuffer[j]);
+            data[j] = dataIn[i][indices[j]];
         }
 
-        // Sort based on the sort order
-        switch(orderingIn[i]){
-            case OrderBy::Order::ASC:
-                std::sort(IKpairs.begin(), IKpairs.end());
-                break;
-            case OrderBy::Order::DESC:
-                std::sort(IKpairs.begin(), IKpairs.end(), std::greater<>());
-                break;
-        }
-
-        // Reorder the output vector indices
+        // 4. Sort the index-data pairs based on data - mind the ordering
+        std::vector<IdxKeyPair<uint32_t>> v(COL_DATA_ELEMENT_COUNT);
         for(int32_t j = 0; j < COL_DATA_ELEMENT_COUNT; j++)
         {
-            indexBuffer[j] = IKpairs[j].second;
+            v[j] = {indices[j], data[j]};
         }
+
+        if(orderingIn[i] == OrderBy::Order::ASC)
+        {
+            stable_sort(v.begin(), v.end(), Asc<uint32_t>());
+        }
+        else 
+        {
+            stable_sort(v.begin(), v.end(), Desc<uint32_t>());
+        }
+
+        // 5. Keep the new index combination
+        for (int32_t j = 0; j < COL_DATA_ELEMENT_COUNT; j++)
+        {
+            indices[j] = v[j].index; 
+        }
+        std::cout << std:: endl;
     }
 
-    // Write the results
+    // 6. Write the results
     std::vector<std::vector<uint32_t>> dataOut;
     for(int32_t i = 0; i < COL_COUNT; i++)
     {
         dataOut.push_back(std::vector<uint32_t>{});
         for(int32_t j = 0; j < COL_DATA_ELEMENT_COUNT; j++)
         {
-            dataOut[i].push_back(dataIn[i][indexBuffer[j]]);
+            // 7. Reaorer by the final indices list
+            dataOut[i].push_back(dataIn[i][indices[j]]);
         }
     }
     
+    /*
     // DEBUG
     std::printf("###############################################################\n");
     std::printf("### CPU ORDER BY ###\n");
@@ -102,6 +155,7 @@ TEST(GPUOrderByTests, GPUOrderByUnsignedTest)
         std::printf("\n");
     }
     //DEBUG END
+    */
 
     /////////////////////////////////////////////////////////////////////////////
     // Sort the input data on the GPU
@@ -146,6 +200,7 @@ TEST(GPUOrderByTests, GPUOrderByUnsignedTest)
     GPUMemory::free(d_resultBuffer);
 
     // Print the results
+    /*
     // DEBUG
     std::printf("###############################################################\n");
     std::printf("### GPU ORDER BY ###\n");
@@ -165,8 +220,15 @@ TEST(GPUOrderByTests, GPUOrderByUnsignedTest)
         std::printf("\n");
     }
     //DEBUG END
+    */
 
     /////////////////////////////////////////////////////////////////////////////
     // Compare the data
-
+    for(int32_t i = 0; i < COL_COUNT; i++)
+    {
+        for(int32_t j = 0; j < COL_DATA_ELEMENT_COUNT; j++)
+        {
+            ASSERT_EQ(dataOut[i][j], dataOutGPU[i][j]);
+        }
+    }
 }
