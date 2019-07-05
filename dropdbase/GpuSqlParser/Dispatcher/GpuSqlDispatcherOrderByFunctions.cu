f5a2e91faca82d84c2c8145751763f6345870339
Nikolas Hamran <nikolas.hamran@instarea.com>
2019-07-05 11:50:56 +0200

    Added few comments for tyep addition

--- dropdbase/GpuSqlParser/Dispatcher/GpuSqlDispatcherOrderByFunctions.cu
+++ dropdbase/GpuSqlParser/Dispatcher/GpuSqlDispatcherOrderByFunctions.cu
@@ -4,6 +4,7 @@
 #include <limits>
 #include <cstdint>
 
+#include "../../DataType.h"
 #include "../../VariantArray.h"
 
 std::array<GpuSqlDispatcher::DispatchFunction, DataType::DATA_TYPE_SIZE> GpuSqlDispatcher::orderByFunctions = { &GpuSqlDispatcher::orderByConst<int32_t>, &GpuSqlDispatcher::orderByConst<int64_t>, &GpuSqlDispatcher::orderByConst<float>, &GpuSqlDispatcher::orderByConst<double>, &GpuSqlDispatcher::invalidOperandTypesErrorHandlerConst<ColmnarDB::Types::Point>, &GpuSqlDispatcher::invalidOperandTypesErrorHandlerConst<ColmnarDB::Types::ComplexPolygon>, &GpuSqlDispatcher::invalidOperandTypesErrorHandlerConst<std::string>, &GpuSqlDispatcher::orderByConst<int8_t>, &GpuSqlDispatcher::orderByCol<int32_t>, &GpuSqlDispatcher::orderByCol<int64_t>, &GpuSqlDispatcher::orderByCol<float>, &GpuSqlDispatcher::orderByCol<double>, &GpuSqlDispatcher::invalidOperandTypesErrorHandlerCol<ColmnarDB::Types::Point>, &GpuSqlDispatcher::invalidOperandTypesErrorHandlerCol<ColmnarDB::Types::ComplexPolygon>, &GpuSqlDispatcher::invalidOperandTypesErrorHandlerCol<std::string>, &GpuSqlDispatcher::orderByCol<int8_t> };
@@ -22,6 +23,21 @@ int32_t GpuSqlDispatcher::freeOrderByTable()
 
 int32_t GpuSqlDispatcher::orderByReconstructRetAllBlocks()
 {
+
+	/*
+	switch(.GetType())
+	{
+		case COLUMN_INT:
+		case COLUMN_LONG:
+		case COLUMN_FLOAT:
+		case COLUMN_DOUBLE:
+		case COLUMN_POINT:
+		case COLUMN_POLYGON:
+		case COLUMN_STRING:
+		case COLUMN_INT8_T:
+	}
+	*/
+
 	if(isOverallLastBlock)
 	{
 		// Count and allocate the result vectors for the output map
