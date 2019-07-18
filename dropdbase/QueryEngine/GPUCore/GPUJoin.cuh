2b15a8ec5304aa2fb07e875468a34235ae011c60
Nikolas Hamran <nikolas.hamran@instarea.com>
2019-07-18 15:26:01 +0200

    added null reorder operations into join WIP

--- dropdbase/QueryEngine/GPUCore/GPUJoin.cuh
+++ dropdbase/QueryEngine/GPUCore/GPUJoin.cuh
@@ -621,7 +621,6 @@ public:
 
 		// Allocan output CPU vector
 		std::vector<T> outBlockVector(resultColumnQJoinIdx[resultColumnQJoinIdxBlockIdx].size());
-		std::vector<uint8_t> outNullBlockVector((resultColumnQJoinIdx[resultColumnQJoinIdxBlockIdx].size() + sizeof(int8_t) * 8 - 1) / (sizeof(int8_t) * 8));
 
 		for (int32_t i = 0; i < resultColumnQJoinIdx[resultColumnQJoinIdxBlockIdx].size(); i++)
 		{
@@ -629,13 +628,7 @@ public:
 			int32_t columnRowId = resultColumnQJoinIdx[resultColumnQJoinIdxBlockIdx][i] % blockSize;
 
 			T val = inColumn.GetBlocksList()[columnBlockId]->GetData()[columnRowId];
-			int8_t nullBit = (inColumn.GetBlocksList()[columnBlockId]->GetNullBitmask()[columnRowId / (sizeof(int8_t) * 8)] >> (columnRowId % (sizeof(int8_t) * 8))) & 1;
-
 			outBlockVector[i] = val;
-
-			nullBit <<= (i % (sizeof(int8_t) * 8));
-			outNullBlockVector[i / 8] |= nullBit;
-
 		}
 
 		outDataSize = outBlockVector.size();
@@ -668,5 +661,69 @@ public:
 			outBlock.push_back(val);
 		}
 		outDataSize = outBlock.size();
+	}
+
+	// Reorder the null mask and put it in a GPU buffer
+	template<typename T>
+	static void reorderNullMaskByJoinTableCPU(int8_t *outNullBlock,
+											  int32_t& outNullBlockSize,
+											  const ColumnBase<T> &inColumn, 
+											  int32_t resultColumnQJoinIdxBlockIdx,
+											  const std::vector<std::vector<int32_t>> &resultColumnQJoinIdx,
+											  int32_t blockSize)
+	{
+		if (resultColumnQJoinIdxBlockIdx < 0 || resultColumnQJoinIdxBlockIdx > resultColumnQJoinIdx.size())
+		{
+			std::cerr << "[ERROR]  Column block index out of bounds" << std::endl;
+		}
+
+		// Allocan output CPU vector
+		std::vector<int8_t> outNullBlockVector((resultColumnQJoinIdx[resultColumnQJoinIdxBlockIdx].size() + sizeof(int8_t) * 8 - 1) / (sizeof(int8_t) * 8));
+
+		for (int32_t i = 0; i < resultColumnQJoinIdx[resultColumnQJoinIdxBlockIdx].size(); i++)
+		{
+			int32_t columnBlockId = resultColumnQJoinIdx[resultColumnQJoinIdxBlockIdx][i] / blockSize;
+			int32_t columnRowId = resultColumnQJoinIdx[resultColumnQJoinIdxBlockIdx][i] % blockSize;
+
+			int8_t nullBit = (inColumn.GetBlocksList()[columnBlockId]->GetNullBitmask()[columnRowId / (sizeof(int8_t) * 8)] >> (columnRowId % (sizeof(int8_t) * 8))) & 1;
+
+			nullBit <<= (i % (sizeof(int8_t) * 8));
+			outNullBlockVector[i / 8] |= nullBit;
+		}
+
+		outNullBlockSize = outNullBlockVector.size();
+
+		GPUMemory::copyHostToDevice(outNullBlock, outNullBlockVector.data(), outNullBlockSize);
+	}
+
+	// Reorder the null mask
+	template<typename T>
+	static void reorderNullMaskByJoinTableCPU(std::vector<int8_t>& outNullBlock,
+											  int32_t& outNullBlockSize,
+											  const ColumnBase<T> &inColumn, 
+											  int32_t resultColumnQJoinIdxBlockIdx,
+											  const std::vector<std::vector<int32_t>> &resultColumnQJoinIdx,
+											  int32_t blockSize)
+	{
+		if (resultColumnQJoinIdxBlockIdx < 0 || resultColumnQJoinIdxBlockIdx > resultColumnQJoinIdx.size())
+		{
+			std::cerr << "[ERROR]  Column block index out of bounds" << std::endl;
+		}
+
+		outNullBlock.clear();
+		outNullBlock.resize((resultColumnQJoinIdx[resultColumnQJoinIdxBlockIdx].size() + sizeof(int8_t) * 8 - 1) / (sizeof(int8_t) * 8));
+
+		for (int32_t i = 0; i < resultColumnQJoinIdx[resultColumnQJoinIdxBlockIdx].size(); i++)
+		{
+			int32_t columnBlockId = resultColumnQJoinIdx[resultColumnQJoinIdxBlockIdx][i] / blockSize;
+			int32_t columnRowId = resultColumnQJoinIdx[resultColumnQJoinIdxBlockIdx][i] % blockSize;
+
+			int8_t nullBit = (inColumn.GetBlocksList()[columnBlockId]->GetNullBitmask()[columnRowId / (sizeof(int8_t) * 8)] >> (columnRowId % (sizeof(int8_t) * 8))) & 1;
+
+			nullBit <<= (i % (sizeof(int8_t) * 8));
+			outNullBlock[i / 8] |= nullBit;
+		}
+
+		outNullBlockSize = outNullBlock.size();
 	}
 };
