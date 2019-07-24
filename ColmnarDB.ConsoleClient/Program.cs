9850f593f6c000b808f80243bf60e9572855ab31
Martin Stano <martin.stano@icloud.com>
2019-07-24 09:46:41 +0200

    WIP implementing console command history

--- ColmnarDB.ConsoleClient/Program.cs
+++ ColmnarDB.ConsoleClient/Program.cs
@@ -12,7 +12,7 @@ namespace ColmnarDB.ConsoleClient
         /// Reads input from console
         /// commands:
         /// use [database]
-        /// query [query]
+        /// [query]
         /// import [file path]
         /// help
         /// exit
@@ -30,9 +30,7 @@ namespace ColmnarDB.ConsoleClient
 
             while (!exit)
             {
-                // wholeCommand = command + parameters
-                Console.Out.Write("> ");
-                string wholeCommand = Console.ReadLine();
+                string wholeCommand = ConsoleExtension.ReadConsole(">");
                 string[] splitCommand = wholeCommand.Split(" ");
 
                 string command = splitCommand[0];
@@ -40,8 +38,12 @@ namespace ColmnarDB.ConsoleClient
                 string database = "";
                 string filePath = "";
 
-                // get parameters
-                if (splitCommand.Length > 1) 
+                if (command != "exit" && command != "use" && command != "import" && command != "help")
+                {
+                    parameters = wholeCommand;
+                    command = "query";
+                } 
+                else if (splitCommand.Length > 1) 
                 {
                     parameters = wholeCommand.Substring(command.Length + 1);
                 }
@@ -93,7 +95,7 @@ namespace ColmnarDB.ConsoleClient
 
                         Console.WriteLine();
                         Console.WriteLine(String.Format(format, "use [database]", "Set current working database"));
-                        Console.WriteLine(String.Format(format, "query [query]", "Run given query"));
+                        Console.WriteLine(String.Format(format, "[query]", "Run given query"));
                         Console.WriteLine(String.Format(format, "import [database] [file path]", "Import given .csv file into database"));
                         Console.WriteLine(String.Format(format, "help", "Show information about commands"));
                         Console.WriteLine(String.Format(format, "exit", "Exit the console"));
@@ -105,8 +107,118 @@ namespace ColmnarDB.ConsoleClient
 
                 }
             }
-
         }
-        
+    }
+
+    public class ConsoleExtension
+    {
+        private static Queue<string> InputQueue = new Queue<string>();
+
+        public static string ReadConsole(string Prompt)
+        {
+            string CommandStringBuilder = "";
+            int CounterOfQueue = 0;
+ 
+            while (true)
+            {
+                // loop until Enter key is pressed
+                ConsoleKeyInfo KeyInfoPressed = Console.ReadKey();
+                switch (KeyInfoPressed.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        // present the member (from end) in the queue not further then the queue size
+                        if (CounterOfQueue == 0)
+                        {
+                            CounterOfQueue = InputQueue.Count;
+                            CommandStringBuilder = InputQueue.ElementAt(CounterOfQueue - 1);
+                            ClearConsole(Prompt, CommandStringBuilder);
+                            CounterOfQueue--;
+                        }
+                        else
+                        {
+                            if (CounterOfQueue == 0)
+                            {
+                                CounterOfQueue = 0;
+                                ClearConsole(Prompt, "");
+                                CommandStringBuilder = "";
+                            }
+                            else
+                            {
+                                CommandStringBuilder = InputQueue.ElementAt(CounterOfQueue - 1);
+                                ClearConsole(Prompt, CommandStringBuilder);
+                                CounterOfQueue--;
+                            }
+                        }
+                        break;
+ 
+                    case ConsoleKey.DownArrow:
+                        // present the member (from begin) in the queue not further then the queue size
+                        if (InputQueue.Count > CounterOfQueue)
+                        {
+                            CommandStringBuilder = InputQueue.ElementAt(CounterOfQueue);
+                            ClearConsole(Prompt, CommandStringBuilder);
+                            CounterOfQueue++;
+                        }
+                        else
+                        {
+                            CounterOfQueue = 0;
+                            ClearConsole(Prompt, "");
+                            CommandStringBuilder = "";
+                        }
+                        break;
+ 
+                    case ConsoleKey.LeftArrow:
+                        // move the cursor to the correct position, not further then the end of the question
+                        if (Console.CursorLeft > Prompt.Length)
+                        {
+                            Console.SetCursorPosition(Console.CursorLeft - 2, Console.CursorTop);
+                        }
+                        break;
+ 
+                    case ConsoleKey.RightArrow:
+                        // move the cursor to the correct position, not further then the end of the buffer
+                        if (Console.CursorLeft < Console.BufferWidth)
+                        {
+                            Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
+                        }
+                        break;
+ 
+                    case ConsoleKey.Backspace:
+                        // Backspace, remove the char until we reach the question, we can't delete the question
+                        if (Console.CursorLeft > Prompt.Length - 1)
+                        {
+                            Console.Write(new string(' ', 1));
+                            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                            CommandStringBuilder = CommandStringBuilder.Remove(CommandStringBuilder.Length - 1, 1);
+                        }
+                        else
+                        {
+                            Console.SetCursorPosition(Prompt.Length, Console.CursorTop);
+                        }
+                        break;
+ 
+                    case ConsoleKey.Delete:
+                        break;
+ 
+                    default:
+                        // just add the char to the answer building string
+                        CommandStringBuilder = CommandStringBuilder + KeyInfoPressed.KeyChar.ToString();
+                        ClearConsole(Prompt, CommandStringBuilder);
+                        break;
+ 
+                    case ConsoleKey.Enter:
+                        // exit this routine and return the Answer to process further
+                        return CommandStringBuilder;
+                }
+            }
+        }
+
+        private static void ClearConsole(string Question, string Answer)
+        {
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new String(' ', Console.BufferWidth - Question.Length));
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(Question + Answer);
+        }
     }
 }
