f4bcebaa673a6728aa2e861097e0625dd9fe89c4
David Kubik <david.kubik@instarea.com>
2019-07-24 13:59:35 +0200

    Change taxi rides queries because of missing functionality

--- ColmnarDB.BenchmarkUtility/taxi_queries.sql
+++ ColmnarDB.BenchmarkUtility/taxi_queries.sql
@@ -1,4 +1,3 @@
-SELECT cab_type, COUNT(*) FROM trips GROUP BY cab_type;
+SELECT cab_type, COUNT(cab_type) FROM trips GROUP BY cab_type;
 SELECT passenger_count, AVG(total_amount) FROM trips GROUP BY passenger_count;
-SELECT passenger_count, YEAR(pickup_datetime) AS pickup_year, COUNT(*) FROM trips GROUP BY passenger_count, pickup_year;	 
-SELECT passenger_count, YEAR(pickup_datetime) AS pickup_year, cast(trip_distance as int) AS distance, COUNT(*) AS the_count FROM trips GROUP BY passenger_count, pickup_year, distance ORDER BY pickup_year, the_count DESC;
+SELECT passenger_count, YEAR(pickup_datetime), COUNT(cab_type) FROM trips GROUP BY passenger_count, cab_type;
