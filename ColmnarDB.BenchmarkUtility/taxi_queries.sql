0ffab75d463e0b7262edb33373f14473e2c10e04
David Kubik <david.kubik@instarea.com>
2019-07-24 17:16:41 +0200

    Temporary change taxi rides queries

--- ColmnarDB.BenchmarkUtility/taxi_queries.sql
+++ ColmnarDB.BenchmarkUtility/taxi_queries.sql
@@ -1,3 +1,2 @@
 SELECT cab_type, COUNT(cab_type) FROM trips GROUP BY cab_type;
-SELECT passenger_count, AVG(total_amount) FROM trips GROUP BY passenger_count;
 SELECT passenger_count, YEAR(pickup_datetime), COUNT(cab_type) FROM trips GROUP BY passenger_count, cab_type;
