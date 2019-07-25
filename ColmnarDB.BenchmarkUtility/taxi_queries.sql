bfcd7d968fa9cdff4a08892646890c9c9d4b5572
Dávid Kubík <david.kubik@instarea.com>
2019-07-25 23:23:02 +0200

    Change taxi rides queries to work

--- ColmnarDB.BenchmarkUtility/taxi_queries.sql
+++ ColmnarDB.BenchmarkUtility/taxi_queries.sql
@@ -1,2 +1,2 @@
-SELECT cab_type, COUNT(cab_type) FROM trips GROUP BY cab_type;
+SELECT COUNT(passenger_count) FROM trips;
 SELECT passenger_count, YEAR(pickup_datetime), COUNT(cab_type) FROM trips GROUP BY passenger_count, cab_type;
