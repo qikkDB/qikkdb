17a1ccda255c08f878f885409a8e17d09788e0a8
David Kubik <david.kubik@instarea.com>
2019-07-26 09:09:13 +0200

    Add hardcore taxi rides queries

--- ColmnarDB.BenchmarkUtility/taxi_queries.sql
+++ ColmnarDB.BenchmarkUtility/taxi_queries.sql
@@ -1,3 +1,4 @@
-SELECT COUNT(passenger_count) FROM trips;
-SELECT passenger_count, cab_type FROM trips GROUP BY passenger_count, cab_type;
-SELECT YEAR(pickup_datetime) FROM trips
+SELECT cab_type, COUNT(passenger_count) FROM trips GROUP BY cab_type;
+SELECT passenger_count, AVG(total_amount) FROM trips GROUP BY passenger_count;
+SELECT passenger_count, YEAR(pickup_datetime), COUNT(passenger_count) FROM trips GROUP BY passenger_count, YEAR(pickup_datetime);	 
+SELECT passenger_count, YEAR(pickup_datetime), cast(trip_distance as int), COUNT(passenger_count) FROM trips GROUP BY passenger_count, YEAR(pickup_datetime), cast(trip_distance as int) ORDER BY YEAR(pickup_datetime), COUNT(passenger_count) DESC;
