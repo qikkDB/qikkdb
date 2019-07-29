d624f39fd67296d5e342aec0c30c75659340eec1
Nikolas Hamran <nikolas.hamran@instarea.com>
2019-07-29 12:37:50 +0200

    Polygon intersection counting added

--- dropdbase/QueryEngine/GPUCore/GPUPolygonClipping.cu
+++ dropdbase/QueryEngine/GPUCore/GPUPolygonClipping.cu
@@ -1,1 +1,88 @@
 #include "GPUPolygonClipping.cuh"
+
+__device__ LLPolyVertex calc_intersect(NativeGeoPoint sA, NativeGeoPoint eA,
+                                       NativeGeoPoint sB, NativeGeoPoint eB)
+{
+    float adx = eA.latitude - sA.latitude;
+    float ady = eA.longitude - sA.longitude;
+    float bdx = eB.latitude - sB.latitude;
+    float bdy = eB.longitude - sB.longitude;
+
+    float axb = adx * bdy - ady * bdx;
+
+    if (axb == 0)
+    {
+        LLPolyVertex retFail = {0, 0, true, false, -1, -1, -1, -1, -1};
+        return retFail;
+    }
+
+    float dx = sA.latitude - sB.latitude;
+    float dy = sA.longitude - sB.longitude;
+
+    float alongA = (bdx * dy - bdy * dx) / axb;
+    float alongB = (adx * dy - ady * dx) / axb;
+
+    bool intersectionValidity = (alongA > 0 && alongA < 1 && alongB > 0 && alongB < 1);
+
+    LLPolyVertex ret = {
+        sA.latitude + alongA * adx,
+        sA.longitude + alongA * ady,
+        true,
+        intersectionValidity,
+        alongA,
+        alongB,
+        -1,
+        -1,
+        -1
+    };
+
+    return ret;
+}
+
+__global__ void kernel_calc_intersection_counts(int32_t *intesection_counts, 
+                                                GPUMemory::GPUPolygon polygonA,
+                                                GPUMemory::GPUPolygon polygonB,
+                                                int32_t dataElementCount)
+{
+    const int32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    const int32_t stride = blockDim.x * gridDim.x;
+
+    for (int32_t i = idx; i < dataElementCount; i += stride)
+    {
+        int32_t polyIdxA = GPUMemory::PolyIdxAt(polygonA, i);
+        int32_t polyCountA = GPUMemory::PolyCountAt(polygonA, i);
+
+        int32_t polyIdxB = GPUMemory::PolyIdxAt(polygonB, i);
+        int32_t polyCountB = GPUMemory::PolyCountAt(polygonB, i);
+
+        // Calcualte the intersection count
+        int32_t intersectCount = 0;
+        for(int32_t a = polyIdxA; a < (polyIdxA + polyCountA); a++)
+        {
+            int32_t pointIdxA = GPUMemory::PointIdxAt(polygonA, a);
+            int32_t pointCountA = GPUMemory::PointCountAt(polygonA, a);
+            for(int32_t b = polyIdxB; b < (polyIdxB + polyCountB); b++)
+            {
+                int32_t pointIdxB = GPUMemory::PointIdxAt(polygonB, b);
+                int32_t pointCountB = GPUMemory::PointCountAt(polygonB, b);
+
+                // Calculate intersections
+                for(int32_t pointA = pointIdxA; pointA < (pointIdxA + pointCountA); pointA++)
+                {
+                    for(int32_t pointB = pointIdxB; pointB < (pointIdxB + pointCountB); pointB++)
+                    {
+                        LLPolyVertex result = calc_intersect(polygonA.polyPoints[pointA], 
+                                                             polygonA.polyPoints[pointIdxA + (pointA + 1) % pointCountA],
+                                                             polygonA.polyPoints[pointB],
+                                                             polygonA.polyPoints[pointIdxB + (pointB + 1) % pointCountB]);
+                        if(result.isValidIntersection)
+                        {
+                            intersectCount++;
+                        }
+                    }       
+                }
+            }   
+        }
+        intesection_counts[i] = intersectCount;
+    }
+}
--- dropdbase/QueryEngine/GPUCore/GPUPolygonClipping.cuh
+++ dropdbase/QueryEngine/GPUCore/GPUPolygonClipping.cuh
@@ -8,10 +8,12 @@
 
 #include "GPUArithmetic.cuh"
 #include "GPUMemory.cuh"
+#include "GPUReconstruct.cuh"
 
 #include "../../../cub/cub.cuh"
 #include "../../NativeGeoPoint.h"
 #include "../Context.h"
+#include "cuda_ptr.h"
 
 
 namespace PolygonFunctions
@@ -33,6 +35,32 @@ namespace PolygonFunctions
     };
 } // namespace PolygonFunctions
 
+// A point in the linked list of polygons
+__device__ struct LLPolyVertex
+{
+    NativeGeoPoint vertex;      // The vertex coordinates
+
+    bool isIntersection;        // Is this an intersection or a polygon vertex
+    bool isValidIntersection;   // Is this a valid interection ? ( does the point lie between the crossing lines)
+
+    float distanceAlongA;       // Distance of the intersection from the beginning of the first line
+    float distanceAlongB;       // Distance of the intersection from the beginning of the second line
+
+    int32_t prevIdx;            // Index of the previous member in the LL
+    int32_t nextIdx;            // Index of the next member in the LL
+    int32_t crossIdx;           // Index in the other complex polygon for cross linking during traversal
+};
+
+// Calcualte an intersection point between two lines
+__device__ LLPolyVertex calc_intersect(NativeGeoPoint sA, NativeGeoPoint eA, 
+                                       NativeGeoPoint sB, NativeGeoPoint eB);
+
+// Calculate the number of intersections between complex polygons for linked list size estimation
+__global__ void kernel_calc_intersection_counts(int32_t *intesection_counts, 
+                                                GPUMemory::GPUPolygon polygonA,
+                                                GPUMemory::GPUPolygon polygonB,
+                                                int32_t dataElementCount);
+
 class GPUPolygonClipping
 {
 public:
@@ -42,6 +70,23 @@ public:
                        GPUMemory::GPUPolygon polygonBin,
                        int32_t dataElementCount)
     {
+        // Create a buffer with the intersection counts and calcualte the intersection counts
+        cuda_ptr<int32_t> intersectionCounts(dataElementCount);
+        cuda_ptr<int32_t> intersectionPrefixSum(dataElementCount);
+        kernel_calc_intersection_counts<<< Context::getInstance().calcGridDim(dataElementCount), 
+                                          Context::getInstance().getBlockDim()>>>(intersectionCounts.get(), 
+                                                                                  polygonAin,
+                                                                                  polygonBin,
+                                                                                  dataElementCount);
+        CheckCudaError(cudaGetLastError());
+
+        // Calculate the prefix sum for the intersection counters for adressing purpose
+        GPUReconstruct::PrefixSum(intersectionPrefixSum.get(), intersectionCounts.get(), dataElementCount);
+
+        // Copy back the result total size
+        int32_t intersectionsTotalCount;
+        GPUMemory::copyDeviceToHost(&intersectionsTotalCount,intersectionPrefixSum.get() + dataElementCount - 1, 1);
+        
         
     }
 };
