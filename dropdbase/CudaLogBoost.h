a543dc41edf2dbba552216fd23df11b16fa3115e
Martin Stano <martin.stano@icloud.com>
2019-08-02 11:00:17 +0200

    impkemented wrapper to support boost trivial log in cuda files

---
+++ dropdbase/CudaLogBoost.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <string>
+
+class CudaLogBoost
+{
+public:
+	enum Severity
+	{
+		trace,
+		debug,
+		info,
+		warning,
+		error,
+	};
+
+	static CudaLogBoost& getInstance(Severity severity)
+	{
+		static CudaLogBoost instanceTrace(Severity::trace);
+		static CudaLogBoost instanceDebug(Severity::debug);
+		static CudaLogBoost instanceInfo(Severity::info);
+		static CudaLogBoost instanceWarning(Severity::warning);
+		static CudaLogBoost instanceError(Severity::error);
+
+		switch (severity)
+		{
+		case trace:
+			return instanceTrace;
+			break;
+		case debug:
+			return instanceDebug;
+			break;
+		case info:
+			return instanceInfo;
+			break;
+		case warning:
+			return instanceWarning;
+			break;
+		case error:
+			return instanceError;
+			break;
+		default:
+			return instanceInfo;
+			break;
+		}
+	}
+
+	CudaLogBoost(CudaLogBoost const&) = delete;
+	void operator=(CudaLogBoost const&) = delete;
+
+	CudaLogBoost& operator<<(const std::string& text);
+
+private:
+	Severity severity;
+	CudaLogBoost(Severity severity);
+};
