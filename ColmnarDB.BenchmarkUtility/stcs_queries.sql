f876a0abe20d6b67b2fb97ee10d9d845667860d6
David Kubik <david.kubik@instarea.com>
2019-08-09 16:38:59 +0200

    Fix queries

--- ColmnarDB.BenchmarkUtility/stcs_queries.sql
+++ ColmnarDB.BenchmarkUtility/stcs_queries.sql
@@ -1,5 +1,5 @@
 SELECT Target.targetId, cellId FROM TargetTraffic JOIN Target ON TargetTraffic.targetId = Target.targetId ORDER BY Target.targetId DESC LIMIT 500 OFFSET 30;
 SELECT cellId, COUNT(genderId) FROM TargetTraffic JOIN Target ON TargetTraffic.targetId = Target.targetId GROUP BY cellId;
-SELECT hwOsId, MIN(TargetTraffic.cellId), MAX(TargetTraffic.eventHour) FROM Target JOIN TargetTraffic ON Target.targetId = TargetTraffic.targetId GROUP BY hwOsId;
+SELECT hwOsId, MIN(TargetTraffic.targetId), MAX(TargetTraffic.eventHour) FROM Target JOIN TargetTraffic ON Target.targetId = TargetTraffic.targetId GROUP BY hwOsId;
 SELECT customerId, COUNT(genderId), MAX(ageId) FROM Target JOIN TargetTraffic ON Target.targetId = TargetTraffic.targetId WHERE TargetTraffic.eventHour > 10 GROUP BY customerId;
 SELECT dataId, TargetTraffic.eventHour FROM Target JOIN TargetTraffic ON Target.targetId = TargetTraffic.targetId WHERE genderId != -1;
