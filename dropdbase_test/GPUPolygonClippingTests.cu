17c304e63b037dc3cc56fec80e00abdefd339247
Nikolas Hamran <nikolas.hamran@instarea.com>
2019-08-12 12:36:40 +0200

    Added intersection and unio tests for WKT polygon clipping

--- dropdbase_test/GPUPolygonClippingTests.cu
+++ dropdbase_test/GPUPolygonClippingTests.cu
@@ -10,92 +10,94 @@
 
 #include "gtest/gtest.h"
 
-TEST(GPUPolygonClippingTests, PoygonTest)
+int32_t dataElementCount = 3;
+
+std::vector<int32_t> polyApolyIdx = {3, 4, 7};
+std::vector<int32_t> polyApointsIdx = {4, 8, 12, 15, 22, 27, 31};
+std::vector<NativeGeoPoint> polyApolyPoints = {
+	{4.50, 5.50},
+	{6.00, 5.50},
+	{6.00, 4.50},
+	{4.50, 4.50},
+
+	{10.00, 0.00},
+	{0.00, 0.00},
+	{0.00, 10.00},
+	{10.00, 10.00},
+
+	{7.00, 7.00},
+	{3.00, 7.00},
+	{3.00, 3.00},
+	{7.00, 3.00},
+
+	{0.00, 0.00},
+	{1.00, 0.00},
+	{0.50, 1.00},
+
+	{-6.31, -1.49},
+	{-4.00, 5.00},
+	{2.13, 6.03},
+	{4.90, 2.23}, 
+	{-0.52, -0.49},
+	{3.88, -3.45},
+	{-4.33, -3.89},
+
+	{-3.77, 2.88},
+	{1.12, 5.24},
+	{3.52, 2.73},
+	{-0.92, 0.45},
+	{-2.82, -2.57},
+
+	{-2.52, 1.91},
+	{0.96, 4.25},
+	{2.16, 2.81},
+	{-1.98, 0.43}
+};
+
+std::vector<int32_t> polyBpolyIdx = {2, 3, 5};
+std::vector<int32_t> polyBpointsIdx = {3, 7, 11, 17, 23};
+std::vector<NativeGeoPoint> polyBpolyPoints = {
+	{13.00, 5.50},
+	{13.00, 4.50},
+	{5.00, 5.00},
+
+	{4.00, 4.00},
+	{15.00, 4.00},
+	{15.00, 6.00},
+	{4.00, 6.00},
+
+	{0.00, 0.40},
+	{1.00, 0.40},
+	{1.00, 0.60},
+	{0.00, 0.60},
+
+	{-5.12, 4.59},
+	{0.42, -5.63},
+	{3.86, 0.41},
+	{2.06, 3.75},
+	{1.22, 6.83},
+	{-4.60, 6.45},
+
+	{-3.32, 4.11},
+	{0.92, 3.69},
+	{2.26, 0.21},
+	{0.00, -3.00}, 
+	{0.48, 1.65}, 
+	{-2.94, 2.27}
+};
+
+template<typename OP>
+void polyTest(std::vector<int32_t> &polyApolyIdx,
+			  std::vector<int32_t> &polyApointsIdx,
+			  std::vector<NativeGeoPoint> &polyApolyPoints,
+			  std::vector<int32_t> &polyBpolyIdx,
+			  std::vector<int32_t> &polyBpointsIdx,
+			  std::vector<NativeGeoPoint> &polyBpolyPoints,
+			  std::vector<int32_t> &outPolyIdx,
+			  std::vector<int32_t> &outPointIdx,
+			  std::vector<NativeGeoPoint> &outPolyPoints,
+			  int32_t dataElementCount)
 {
-	int32_t dataElementCount = 3;
-
-	std::vector<int32_t> polyApolyIdx = {3, 4, 7};
-	std::vector<int32_t> polyApointsIdx = {4, 8, 12, 15, 22, 27, 31};
-	std::vector<NativeGeoPoint> polyApolyPoints = {
-		{4.5, 5.5},
-		{6.0, 5.5},
-		{6.0, 4.5},
-		{4.5, 4.5},
-
-		{10.0, 0.0},
-		{0.0, 0.0},
-		{0.0, 10.0},
-		{10.0, 10.0},
-
-		{7.0, 7.0},
-		{3.0, 7.0},
-		{3.0, 3.0},
-		{7.0, 3.0},
-
-		{0.0, 0.0},
-		{1.0, 0.0},
-		{0.5, 1.0},
-
-		{-6.31, -1.49},
-		{-4, 5},
-		{2.13, 6.03},
-		{4.9, 2.23}, 
-		{-0.52, -0.49},
-		{3.88, -3.45},
-		{-4.33, -3.89},
-
-		{-3.77, 2.88},
-		{1.12, 5.24},
-		{3.52, 2.73},
-		{-0.92, 0.45},
-		{-2.82, -2.57},
-
-		{-2.52, 1.91},
-		{0.96, 4.25},
-		{2.16, 2.81},
-		{-1.98, 0.43}
-	};
-
-	std::vector<int32_t> polyBpolyIdx = {2, 3, 5};
-	std::vector<int32_t> polyBpointsIdx = {3, 7, 11, 17, 23};
-	std::vector<NativeGeoPoint> polyBpolyPoints = {
-		{13.0, 5.5},
-		{13.0, 4.5},
-		{5.0, 5.0},
-
-		{4.0, 4.0},
-		{15.0, 4.0},
-		{15.0, 6.0},
-		{4.0, 6.0},
-
-		{0.0, 0.4},
-		{1.0, 0.4},
-		{1.0, 0.6},
-		{0.0, 0.6},
-
-		{-5.12, 4.59},
-		{0.42, -5.63},
-		{3.86, 0.41},
-		{2.06, 3.75},
-		{1.22, 6.83},
-		{-4.6, 6.45},
-
-		{-3.32, 4.11},
-		{0.92, 3.69},
-		{2.26, 0.21},
-		{0, -3}, 
-		{0.48, 1.65}, 
-		{-2.94, 2.27}
-	};
-
-	std::vector<int32_t> outPolyIdxCorrect = {};
-	std::vector<int32_t> outPointIdxCorrect = {};
-	std::vector<NativeGeoPoint> outPolyPointsCorrect = {};
-
-	std::vector<int32_t> outPolyIdx;
-	std::vector<int32_t> outPointIdx;
-	std::vector<NativeGeoPoint> outPolyPoints;
-
 	// Alloc the GPU structures
 	GPUMemory::GPUPolygon polygonA;
 	GPUMemory::GPUPolygon polygonB;
@@ -119,7 +121,7 @@ TEST(GPUPolygonClippingTests, PoygonTest)
 
 	// Perform the polygon intersect
 	GPUMemory::GPUPolygon polygonOut;	// This needs to be empty
-	GPUPolygonClipping::ColCol<PolygonFunctions::polyIntersect>(polygonOut, polygonA, polygonB, dataElementCount);
+	GPUPolygonClipping::ColCol<OP>(polygonOut, polygonA, polygonB, dataElementCount);
 
 	// Copy back the results
 	outPolyIdx.resize(dataElementCount);
@@ -135,26 +137,226 @@ TEST(GPUPolygonClippingTests, PoygonTest)
 	GPUMemory::free(polygonA);
 	GPUMemory::free(polygonB);
 	GPUMemory::free(polygonOut);
+}
+
+float roundCustom(float var) 
+{ 
+    float value = (int)(var * 100 + .5); 
+    return (float)value / 100; 
+} 
+
+TEST(GPUPolygonClippingTests, IntersectTest)
+{
+	std::vector<int32_t> outPolyIdx;
+	std::vector<int32_t> outPointIdx;
+	std::vector<NativeGeoPoint> outPolyPoints;
+
+	// Run the intersect test
+	polyTest<PolygonFunctions::polyIntersect>(
+		polyApolyIdx,
+		polyApointsIdx,
+		polyApolyPoints,
+		polyBpolyIdx,
+		polyBpointsIdx,
+		polyBpolyPoints,
+		outPolyIdx,
+		outPointIdx,
+		outPolyPoints,
+		dataElementCount);
+
+	std::vector<int32_t> outPolyIdxCorrect = {3, 4, 9 };
+	std::vector<int32_t> outPointIdxCorrect = {7, 11, 15, 19, 29, 33, 44, 49, 53 };
+	std::vector<NativeGeoPoint> outPolyPointsCorrect = {
+		{6.000000, 4.937500},
+		{6.000000, 4.500000},
+		{4.500000, 4.500000},
+		{4.500000, 5.500000},
+		{6.000000, 5.500000},
+		{6.000000, 5.062500},
+		{5.000000, 5.000000},
+		{10.000000, 4.687500},
+		{10.000000, 4.000000},
+		{7.000000, 4.000000},
+		{7.000000, 4.875000},
+		{10.000000, 5.312500},
+		{10.000000, 6.000000},
+		{7.000000, 6.000000},
+		{7.000000, 5.125000},
+		{0.800000, 0.400000},
+		{0.700000, 0.600000},
+		{0.300000, 0.600000},
+		{0.200000, 0.400000},
+		{-4.532012, 3.505300},
+		{-4.000000, 5.000000},
+		{1.468495, 5.918850},
+		{1.866582, 4.459199},
+		{1.120000, 5.240000},
+		{-1.578781, 3.937521},
+		{-3.320000, 4.110000},
+		{-3.129789, 3.188977},
+		{-3.770000, 2.880000},
+		{-3.569482, 1.729657},
+		{3.309359, 1.431745},
+		{2.035682, 0.792556},
+		{1.657860, 1.773766},
+		{2.807015, 2.363873},
+		{2.276109, -2.371018},
+		{1.032058, -1.534112},
+		{0.000000, -3.000000},
+		{0.208507, -0.980087},
+		{-0.520000, -0.490000},
+		{0.301662, -0.077653},
+		{0.427560, 1.141991},
+		{-0.920000, 0.450000},
+		{-1.970601, -1.219902},
+		{-0.630680, -3.691742},
+		{1.591744, -3.572635},
+		{0.228978, 3.758451},
+		{0.960000, 4.250000},
+		{2.160000, 2.810000},
+		{1.422176, 2.385841},
+		{0.920000, 3.690000},
+		{-2.187495, 2.133581},
+		{-2.520000, 1.910000},
+		{-1.980000, 0.430000},
+		{0.223174, 1.696559}
+	};
+
 
 	// Check the results
-
-	// DEBUG
+	ASSERT_EQ(outPolyIdx.size(), outPolyIdxCorrect.size());
 	for(int32_t i = 0; i < outPolyIdx.size(); i++)
 	{
-		printf("%d : %d\n", i, outPolyIdx[i]);
+		ASSERT_EQ(outPolyIdx[i], outPolyIdxCorrect[i]);
 	}
-	printf("\n");
 
+	ASSERT_EQ(outPointIdx.size(), outPointIdxCorrect.size());
 	for(int32_t i = 0; i < outPointIdx.size(); i++)
 	{
-		printf("%d : %d\n", i, outPointIdx[i]);
+		ASSERT_EQ(outPointIdx[0], outPointIdxCorrect[0]);
 	}
-	printf("\n");
 
+	ASSERT_EQ(outPolyPoints.size(), outPolyPointsCorrect.size());
 	for(int32_t i = 0; i < outPolyPoints.size(); i++)
 	{
-		printf("%d : %.2f %.2f\n", i, outPolyPoints[i].latitude,outPolyPoints[i].longitude);
+		ASSERT_FLOAT_EQ(roundCustom(outPolyPoints[i].latitude), roundCustom(outPolyPointsCorrect[i].latitude));
+		ASSERT_FLOAT_EQ(roundCustom(outPolyPoints[i].longitude), roundCustom(outPolyPointsCorrect[i].longitude));
 	}
-	printf("\n");
+}
 
+TEST(GPUPolygonClippingTests, UnionTest)
+{
+	std::vector<int32_t> outPolyIdx;
+	std::vector<int32_t> outPointIdx;
+	std::vector<NativeGeoPoint> outPolyPoints;
+
+	// Run the union test
+	polyTest<PolygonFunctions::polyUnion>(polyApolyIdx,
+		polyApointsIdx,
+		polyApolyPoints,
+		polyBpolyIdx,
+		polyBpointsIdx,
+		polyBpolyPoints,
+		outPolyIdx,
+		outPointIdx,
+		outPolyPoints,
+		dataElementCount);
+
+	std::vector<int32_t> outPolyIdxCorrect = {4, 5, 11};
+	std::vector<int32_t> outPointIdxCorrect = {4, 8, 16, 24, 35, 51, 56, 60, 63, 68, 73};
+	std::vector<NativeGeoPoint> outPolyPointsCorrect = {
+		{6.000000, 4.937500},
+		{6.000000, 5.062500},
+		{7.000000, 5.125000},
+		{7.000000, 4.875000},
+		{10.000000, 4.687500},
+		{10.000000, 5.312500},
+		{13.000000, 5.500000},
+		{13.000000, 4.500000},
+		{10.000000, 4.000000},
+		{10.000000, 0.000000},
+		{0.000000, 0.000000},
+		{0.000000, 10.000000},
+		{10.000000, 10.000000},
+		{10.000000, 6.000000},
+		{15.000000, 6.000000},
+		{15.000000, 4.000000},
+		{7.000000, 4.000000},
+		{7.000000, 3.000000},
+		{3.000000, 3.000000},
+		{3.000000, 7.000000},
+		{7.000000, 7.000000},
+		{7.000000, 6.000000},
+		{4.000000, 6.000000},
+		{4.000000, 4.000000},
+		{0.800000, 0.400000},
+		{1.000000, 0.000000},
+		{0.000000, 0.000000},
+		{0.200000, 0.400000},
+		{0.000000, 0.400000},
+		{0.000000, 0.600000},
+		{0.300000, 0.600000},
+		{0.500000, 1.000000},
+		{0.700000, 0.600000},
+		{1.000000, 0.600000},
+		{1.000000, 0.400000},
+		{-4.532012, 3.505300},
+		{-6.310000, -1.490000},
+		{-4.330000, -3.890000},
+		{-0.630680, -3.691742},
+		{0.420000, -5.630000},
+		{1.591744, -3.572635},
+		{3.880000, -3.450000},
+		{2.276109, -2.371018},
+		{3.860000, 0.410000},
+		{3.309359, 1.431745},
+		{4.900000, 2.230000},
+		{2.130000, 6.030000},
+		{1.468495, 5.918850},
+		{1.220000, 6.830000},
+		{-4.600000, 6.450000},
+		{-5.120000, 4.590000},
+		{2.035682, 0.792556},
+		{0.301662, -0.077653},
+		{0.208507, -0.980087},
+		{1.032058, -1.534112},
+		{2.260000, 0.210000},
+		{1.866582, 4.459199},
+		{3.520000, 2.730000},
+		{2.807015, 2.363873},
+		{2.060000, 3.750000},
+		{-1.970601, -1.219902},
+		{-2.820000, -2.570000},
+		{-3.569482, 1.729657},
+		{-1.578781, 3.937521},
+		{-3.129789, 3.188977},
+		{-2.940000, 2.270000},
+		{-2.187495, 2.133581},
+		{0.228978, 3.758451},
+		{1.657860, 1.773766},
+		{0.427560, 1.141991},
+		{0.480000, 1.650000},
+		{0.223174, 1.696559},
+		{1.422176, 2.385841}
+		};
+
+	// Check the results
+	ASSERT_EQ(outPolyIdx.size(), outPolyIdxCorrect.size());
+	for(int32_t i = 0; i < outPolyIdx.size(); i++)
+	{
+		ASSERT_EQ(outPolyIdx[i], outPolyIdxCorrect[i]);
+	}
+
+	ASSERT_EQ(outPointIdx.size(), outPointIdxCorrect.size());
+	for(int32_t i = 0; i < outPointIdx.size(); i++)
+	{
+		ASSERT_EQ(outPointIdx[0], outPointIdxCorrect[0]);
+	}
+
+	ASSERT_EQ(outPolyPoints.size(), outPolyPointsCorrect.size());
+	for(int32_t i = 0; i < outPolyPoints.size(); i++)
+	{
+		ASSERT_FLOAT_EQ(roundCustom(outPolyPoints[i].latitude), roundCustom(outPolyPointsCorrect[i].latitude));
+		ASSERT_FLOAT_EQ(roundCustom(outPolyPoints[i].longitude), roundCustom(outPolyPointsCorrect[i].longitude));
+	}
 }
