a9aa8a809a91bf2592fd6578abe4e4b9dabd4b59
David Kubik <david.kubik@instarea.com>
2019-08-14 15:11:52 +0200

    Change queries count(passenger_count) to count(*)

--- ColmnarDB.BenchmarkUtility/taxi_queries.sql
+++ ColmnarDB.BenchmarkUtility/taxi_queries.sql
@@ -1,4 +1,4 @@
-SELECT cab_type, COUNT(passenger_count) FROM trips GROUP BY cab_type;
+SELECT cab_type, COUNT(*) FROM trips GROUP BY cab_type;
 SELECT passenger_count, AVG(total_amount) FROM trips GROUP BY passenger_count;
-SELECT passenger_count, YEAR(pickup_datetime) AS pickup_year, COUNT(passenger_count) FROM trips GROUP BY passenger_count, pickup_year;
-SELECT passenger_count, YEAR(pickup_datetime) AS pickup_year, CAST(trip_distance AS INT) AS distance, COUNT(passenger_count) AS the_count FROM trips GROUP BY passenger_count, pickup_year, distance ORDER BY pickup_year, the_count DESC;
+SELECT passenger_count, YEAR(pickup_datetime) AS pickup_year, COUNT(*) FROM trips GROUP BY passenger_count, pickup_year;
+SELECT passenger_count, YEAR(pickup_datetime) AS pickup_year, CAST(trip_distance AS INT) AS distance, COUNT(*) AS the_count FROM trips GROUP BY passenger_count, pickup_year, distance ORDER BY pickup_year, the_count DESC;
