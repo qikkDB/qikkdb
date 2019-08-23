d7c7d56fd5c1e0d91262616fa793d9d3a6ce5b74
Veronika Vcelkova <veronika.vcelkova@instarea.com>
2019-08-23 15:19:37 +0200

    cast decimal implementation

--- dropdbase/QueryEngine/GPUCore/GPUCast.cu
+++ dropdbase/QueryEngine/GPUCore/GPUCast.cu
@@ -20,3 +20,68 @@ __device__ int32_t CastOperations::FromString::operator()<int32_t>(char* str, in
 {
     return CastIntegral(str, length);
 }
+
+__device__ double CastDecimal(char* str, int32_t length)
+{
+    double out = 0;
+    int decimalPart = 0;
+
+    int c;
+    while (((c = *str++) >= '0' && c <= '9') && length > 0)
+    {
+        out *= 10 + (c - '0');
+        length--;
+    }
+
+    if ((c == '.' || c == ',') && length > 0)
+    {
+        length--;
+        while (((c = *str++) >= '0' && c <= '9') && length > 0)
+        {
+            out *= 10 + (c - '0');
+            decimal_part--;
+            length--;
+        }
+    }
+
+    else if ((c == 'e' || c == 'E') && length > 0)
+    {
+        length--;
+        int sign = 1;
+        int aferEPart = 0;
+
+		c = *s++;
+        if (c == '-')
+        {
+            sign = -1;
+            length--;
+        }
+
+		else if (c == '+')
+        {
+            length--;
+        }
+
+        while (((c == *str++) >= '0' && c <= '9') && length > 0)
+        {
+            afterEPart = afterEPart * 10 + (c - '0');
+            length--;
+        }
+
+        decimalPart += afterEPart * sign;
+    }
+
+    while (decimalPart > 0)
+    {
+        out *= 10;
+        decimalPart--;
+	}
+
+	while (decimalPart < 0)
+    {
+        out *= 0.1;
+        decimalPart++;
+    }
+
+    return out;
+}
