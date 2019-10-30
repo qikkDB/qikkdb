f2fb06b6630bab2316ef6020f9af11e4582891e8
Nikolas Hamran <nikolas.hamran@instarea.com>
2019-10-30 15:34:40 +0100

    Added simple merge join test

--- dropdbase_test/GPUMergeJoinTests.cu
+++ dropdbase_test/GPUMergeJoinTests.cu
@@ -10,76 +10,48 @@
 TEST(GPUMergeJoinTests, MergeJoinTest)
 {
 	// Initialize test buffers
-    const int32_t BLOCK_COUNT_A = 13; // 1;
-    const int32_t BLOCK_SIZE_A = 1 << 25; // 13;	
+    const int32_t BLOCK_COUNT_A = 1;
+    const int32_t BLOCK_SIZE_A = 13;	
 
-	const int32_t BLOCK_COUNT_B = 13; // 1;
-    const int32_t BLOCK_SIZE_B = 1 << 25; // 8;	
+	const int32_t BLOCK_COUNT_B = 1;
+    const int32_t BLOCK_SIZE_B = 8;	
 
 	ColumnBase<int32_t> colA("ColA", BLOCK_SIZE_A);
 	ColumnBase<int32_t> colB("ColB", BLOCK_SIZE_B);
-	
-	for (int32_t i = 0; i < BLOCK_COUNT_A; i++)
-	{
-		auto& blockA = colA.AddBlock();
 
-		std::vector<int32_t> colAData;
-		for (int32_t j = 0; j < BLOCK_SIZE_A; j++)
-		{
-            colAData.push_back(i * BLOCK_SIZE_A + j);
-		}
-
-		blockA.InsertData(colAData);
-	}
-
-	for (int32_t i = 0; i < BLOCK_COUNT_B; i++)
-	{
-		auto& blockB = colB.AddBlock();
-
-		std::vector<int32_t> colBData;
-		for (int32_t j = 0; j < BLOCK_SIZE_B; j++)
-		{
-            colBData.push_back(i * BLOCK_SIZE_B + j);
-		}
-
-		blockB.InsertData(colBData);
-	}
-	
-    
-	/*
     auto& blockA = colA.AddBlock();
     auto& blockB = colB.AddBlock();
 
-    std::vector<int32_t> colAData = {'b', 'c', 'g', 'c', 'd', 'a', 'e',
-                                     'a', 'e', 'c', 'f', 'g', 'c'
-    };
-    std::vector<int32_t> colBData = {
-        'd', 'g', 'e', 'b', 'f', 'h', 'c', 'a'
-    };
+    std::vector<int32_t> colAData = {'b', 'c', 'g', 'c', 'd', 'a', 'e', 'a', 'e', 'c', 'f', 'g', 'c'};
+    std::vector<int32_t> colBData = {'d', 'g', 'e', 'b', 'f', 'h', 'c', 'a'};
 
 	blockA.InsertData(colAData);
     blockB.InsertData(colBData);
-	*/
 
 	// Initialize th output buffers
     std::vector<std::vector<int32_t>> colAJoinIndices;
     std::vector<std::vector<int32_t>> colBJoinIndices;
 
 	// Perform the merge join
-	auto start = std::chrono::steady_clock::now();
+	//auto start = std::chrono::steady_clock::now();
 
 	MergeJoin::JoinUnique(colAJoinIndices, colBJoinIndices, colA, colB);
 
-	auto end = std::chrono::steady_clock::now();
-	std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
+	//auto end = std::chrono::steady_clock::now();
+	//std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
 
-	// DEBUG
-    /*
-    for (int32_t x = 0; x < colAJoinIndices.size(); x++)
+	std::vector<int32_t> colAJoinIndicesCorrect = {5, 7, 0, 1, 3, 9, 12, 4, 6, 8, 10, 2, 11};
+    std::vector<int32_t> colBJoinIndicesCorrect = {7, 7, 3, 6, 6, 6, 6, 0, 2, 2, 4, 1, 1};
+
+	ASSERT_EQ(colAJoinIndices[0].size(), colAJoinIndicesCorrect.size());
+    for (int32_t i = 0; i < colAJoinIndices[0].size(); i++)
     {
-        std::printf("%3d : %3d %3d\n", x , colAJoinIndices[x], colBJoinIndices[x]);
+        ASSERT_EQ(colAJoinIndices[0][i], colAJoinIndicesCorrect[i]);
 	}
-	*/
 
-    FAIL();
+	ASSERT_EQ(colBJoinIndices[0].size(), colBJoinIndicesCorrect.size());
+    for (int32_t i = 0; i < colBJoinIndices[0].size(); i++)
+    {
+        ASSERT_EQ(colBJoinIndices[0][i], colBJoinIndicesCorrect[i]);
+    }
 }
