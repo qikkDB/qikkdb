aa034b8c3e8f4b1e67848448c09595a8ae310e6a
Martin Stano <martin.stano@instarea.com>
2020-05-22 18:48:55 +0200

    Community version: moved Context method implementations to .cu file enabling gpu count limitation

---
+++ dropdbase/QueryEngine/Context.cu
@@ -0,0 +1,199 @@
+#include "Context.h"
+
+Context::Context()
+{
+    Initialize();
+}
+
+Context::~Context()
+{
+    gpuCaches_.clear();
+    gpuAllocators_.clear();
+    for (int32_t i = 0; i < deviceCount_; i++)
+    {
+        // Bind device and clean up
+        bindDeviceToContext(i);
+        cudaDeviceReset();
+    }
+}
+
+void Context::Initialize()
+{
+    const cudaError_t err = cudaGetDeviceCount(&deviceCount_);
+    if (err != cudaSuccess)
+    {
+        CudaLogBoost::getInstance(CudaLogBoost::error)
+            << "cudaGetDeviceCount returns " << err << " which is " << cudaGetErrorName(err) << '\n';
+        throw std::invalid_argument("ERROR: Unable to get device count");
+    }
+
+    // DANGER DANGER DANGER !!! DEADLY DEADLY DEADLY !!!
+    // Don't touch anymore :D Use config UsingMultipleGPUs instead
+    if (!Configuration::GetInstance().IsUsingMultipleGPUs())
+    {
+        deviceCount_ = 1;
+    }
+#ifdef COMMUNITY
+    deviceCount_ = Configuration::GetInstance().GetGpusLimit();
+#endif // COMMUNITY
+    const int cachePercentage = Configuration::GetInstance().GetGPUCachePercentage();
+    CudaLogBoost::getInstance(CudaLogBoost::info) << "Initializing CUDA devices..." << '\n';
+    CudaLogBoost::getInstance(CudaLogBoost::info) << "Found " << deviceCount_ << " CUDA devices" << '\n';
+
+    // Get devices information
+    for (int32_t i = 0; i < deviceCount_; i++)
+    {
+        // Bind device and initialize everything for a device allocators/cache
+        bindDeviceToContext(i);
+
+        // Get devices information
+        cudaDeviceProp deviceProp;
+        if (cudaGetDeviceProperties(&deviceProp, i) != cudaSuccess)
+        {
+            throw std::invalid_argument("ERROR: Failed to get GPU info");
+        }
+        devicesMetaInfoList_.push_back(deviceProp);
+        // Print memory info
+        size_t free, total;
+        cudaMemGetInfo(&free, &total);
+        CudaLogBoost::getInstance(CudaLogBoost::info) << "Initializing memory for device " << i << "\n";
+        // Initialize allocators
+        gpuAllocators_.emplace_back(std::make_unique<CudaMemAllocator>(i));
+        CudaLogBoost::getInstance(CudaLogBoost::info) << "Initializing cache for device " << i << "\n";
+        // Initialize cache
+        size_t cacheSize = static_cast<int64_t>(free * static_cast<double>(cachePercentage) / 100.0);
+        gpuCaches_.emplace_back(std::make_unique<GPUMemoryCache>(i, cacheSize));
+
+        // Get the correct blockDim from the device - use always based on the bound device - optimal for kernels
+        queriedBlockDimensionList.push_back(deviceProp.maxThreadsPerBlock);
+
+        // Print device info
+        CudaLogBoost::getInstance(CudaLogBoost::info)
+            << "Device " << i << " Initialization done " << deviceProp.name
+            << "\t maxBlockDim: " << deviceProp.maxThreadsPerBlock << "\n";
+
+        CudaLogBoost::getInstance(CudaLogBoost::info) << "Memory: Total: " << total << " B Free: " << free
+                                                      << "B Cache: " << cacheSize << " B" << '\n';
+    }
+
+    // Bind default device and notify the user
+    bindDeviceToContext(DEFAULT_DEVICE_ID);
+    CudaLogBoost::getInstance(CudaLogBoost::info) << "Bound default device ID: " << getBoundDeviceID() << '\n';
+
+    // Enable peer to peer communication of each GPU to the default device
+    // This operation is unidirectional
+    for (int32_t i = 0; i < deviceCount_; i++)
+    {
+        if (i != DEFAULT_DEVICE_ID)
+        {
+            int32_t canAccessPeer;
+            if (cudaDeviceCanAccessPeer(&canAccessPeer, DEFAULT_DEVICE_ID, i) != cudaSuccess)
+            {
+                throw std::invalid_argument("ERROR: CUDA peer acces not supported");
+            }
+            if (canAccessPeer == 1)
+            {
+                cudaDeviceEnablePeerAccess(i, DEFAULT_DEVICE_ID);
+            }
+        }
+    }
+}
+
+Context& Context::getInstance()
+{
+    // Static instance - constructor called only once
+    static Context instance;
+    return instance;
+}
+
+int32_t Context::calcGridDim(size_t dataElementCount)
+{
+    if (dataElementCount <= 0)
+    {
+        CheckQueryEngineError(QueryEngineErrorType::GPU_EXTENSION_ERROR,
+                              "Data Element Count must be > 0");
+    }
+    int blockCount = (dataElementCount + getBlockDim() - 1) / getBlockDim();
+    if (blockCount >= (DEFAULT_GRID_DIMENSION_LIMIT + 1))
+    {
+        blockCount = DEFAULT_GRID_DIMENSION_LIMIT;
+    }
+    return blockCount;
+}
+
+int32_t Context::getBlockDim() const
+{
+    return queriedBlockDimensionList[getBoundDeviceID()];
+}
+
+int32_t Context::getBlockDimPoly() const
+{
+    return queriedBlockDimensionList[getBoundDeviceID()] / 2;
+}
+
+int32_t Context::getBoundDeviceID() const
+{
+    int boundDeviceID = -1;
+    cudaGetDevice(&boundDeviceID);
+    return boundDeviceID;
+}
+
+int32_t Context::getDeviceCount() const
+{
+    return deviceCount_;
+}
+
+const std::vector<cudaDeviceProp>& Context::getDevicesMetaInfoList() const
+{
+    return devicesMetaInfoList_;
+}
+
+void Context::bindDeviceToContext(int32_t deviceID)
+{
+    // Check for invalid range
+    if (deviceID < 0 || deviceID >= deviceCount_)
+    {
+        throw std::out_of_range("ERROR: Device ID not present");
+    }
+    cudaDeviceSynchronize();
+    auto error = cudaSetDevice(deviceID);
+    cudaDeviceSynchronize();
+    CheckCudaError(error);
+}
+
+CudaMemAllocator& Context::GetAllocatorForDevice(int32_t deviceID)
+{
+    // Check for invalid range
+    if (deviceID < 0 || deviceID >= deviceCount_)
+    {
+        throw std::out_of_range("ERROR: Device ID not present");
+    }
+
+    return *gpuAllocators_.at(deviceID);
+}
+
+CudaMemAllocator& Context::GetAllocatorForCurrentDevice()
+{
+    return *gpuAllocators_.at(getBoundDeviceID());
+}
+
+GPUMemoryCache& Context::getCacheForDevice(int32_t deviceID)
+{
+    // Check for invalid range
+    if (deviceID < 0 || deviceID >= deviceCount_)
+    {
+        throw std::out_of_range("ERROR: Device ID not present");
+    }
+
+    return *gpuCaches_.at(deviceID);
+}
+
+GPUMemoryCache& Context::getCacheForCurrentDevice()
+{
+    return *gpuCaches_.at(getBoundDeviceID());
+}
+
+std::unordered_map<std::string, std::shared_ptr<Database>>& Context::GetLoadedDatabases()
+{
+    return loadedDatabases_;
+}
