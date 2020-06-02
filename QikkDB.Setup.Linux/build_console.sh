d6d529965679db2c09a28884848913729ca1467d
Martin Stano <martin.stano@instarea.com>
2020-06-02 23:24:54 +0200

    Qikk rename: fixed build console script

--- QikkDB.Setup.Linux/build_console.sh
+++ QikkDB.Setup.Linux/build_console.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPTPATH=$(dirname $(realpath $0) )
-cd "$SCRIPTPATH"/../ColmnarDB.ConsoleClient
+cd "$SCRIPTPATH"/../QikkDB.ConsoleClient
 dotnet publish -c Release -r linux-x64 -o ../publish/console
